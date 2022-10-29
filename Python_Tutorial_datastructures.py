d19448839fb474825cf83b0796fcc2cda78d6379
Lina-Liuna <liuna.lina@gmail.com>
2022-10-29 07:14:03 -0700

    Add sets examples

--- Python_Tutorial_datastructures.py
+++ Python_Tutorial_datastructures.py
@@ -102,6 +102,28 @@ print(u)
 # t[0] = 10000 # Tuples are immutable, tuple object does not support item assginment.
 print(t)
 
+#Sets
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)  #show that duplicates have been removed
+print('orange' in basket)   #fast membership testing
+print('crabgrass' in basket)
+
+#Demonstrate set operations on unique letters from two words
+a = set('abracadabra')
+b = set('alacazam')
+print(a)      #unique letters in a
+print(b)      #unique letters in a
+print(a-b)    #letters in a but not in b
+print(a | b)  #letters in a or b or both
+print(a & b)  # letters in both a and b
+print(a ^ b)  # letters in a or b but not both
+
+
+a = {x for x in 'abracadabra' if x not in 'abc'}
+print(a)
+
+
+
 
 
 
