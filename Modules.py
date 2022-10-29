701ee4bb8039833557733570eecfafcb704227e3
Lina-Liuna <liuna.lina@gmail.com>
2022-10-29 14:28:58 -0700

    How to import module

--- Modules.py
+++ Modules.py
@@ -5,3 +5,15 @@ print(fibo.__name__)
 
 fib = fibo.fib
 fib(500)
+
+from fibo import fib, fib2
+fib(500)
+
+from fibo import *
+fib(500)
+
+import fibo as fib
+fib.fib(500)
+
+from fibo import fib as fibonacci
+fibonacci(500)
