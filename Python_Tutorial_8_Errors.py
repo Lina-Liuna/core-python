dc62506194a188a77219e3eebe4c3893a12d7d5b
Lina-Liuna <liuna.lina@gmail.com>
2022-11-01 14:26:43 -0700

    How try except clause works?

--- Python_Tutorial_8_Errors.py
+++ Python_Tutorial_8_Errors.py
@@ -6,5 +6,24 @@
 #print(10 *(1/0))
 #ZeroDivisionError: division by zero
 
-print('2' + 2)
+#print('2' + 2)
 #TypeError: can only concatenate str (not "int") to str
+
+
+#Handling Exceptions
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print('Oops! That was no valid number. Try again...')
+
+
+#An except clause may name multiple exceptions as a parenthesized tuple
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except (RuntimeError, TabError, NameError):
+        #print('Oops! That was no valid number. Try again...')
+        pass
