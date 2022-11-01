7590751431bceafdee623f4b8b896aa1d6885200
Lina-Liuna <liuna.lina@gmail.com>
2022-11-01 06:31:06 -0700

    Two types of Errors:Syntax errors and exceptions, what is traceback?

---
+++ Python_Tutorial_8_Errors.py
@@ -0,0 +1,10 @@
+#Syntax Errors
+#while True print("hello, world")
+#SyntaxError: invalid syntax
+
+#Exceptions
+#print(10 *(1/0))
+#ZeroDivisionError: division by zero
+
+print('2' + 2)
+#TypeError: can only concatenate str (not "int") to str
