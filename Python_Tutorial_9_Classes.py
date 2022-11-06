d8599908acb9da1727fda5d8c45b86d61f107209
Lina-Liuna <liuna.lina@gmail.com>
2022-11-06 14:10:12 -0800

    how to add iterator behavior into your class?

--- Python_Tutorial_9_Classes.py
+++ Python_Tutorial_9_Classes.py
@@ -186,7 +186,31 @@ print(it)
 print(next(it))
 print(next(it))
 print(next(it))
-print(next(it))
+#print(next(it))
+
+#how to add iterator behavior to your classes?
+class Reverse:
+    """Iterator for looping over a sequence backwards."""
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    #if the class defines __next__(), then __iter__() can just return self
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+rev = Reverse('spam')
+print(iter(rev))
+
+for char in rev:
+    print(char)
+
 
 
 
