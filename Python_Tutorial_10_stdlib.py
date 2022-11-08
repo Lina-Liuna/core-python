d07ebf69e32dc6379b7a54bd060b527a73b5b904
Lina-Liuna <liuna.lina@gmail.com>
2022-11-08 09:00:34 -0800

    using datetime module to manipulating and formatting dates and times

--- Python_Tutorial_10_stdlib.py
+++ Python_Tutorial_10_stdlib.py
@@ -108,3 +108,13 @@ import smtplib
 #                Winter time and rainny days
 #                """)
 #server.quit()
+
+from datetime import date
+now = date.today()
+print(now)
+
+print(now.strftime('%m-%d-%y. %d %b %Y is a %A on the %d day of %B'))
+
+birthday = date(1990, 1, 1)
+age = now - birthday
+print(age)
