4fdb751668d22d360e27e0e6a992fd4d13a27703
Lina-Liuna <liuna.lina@gmail.com>
2022-11-09 07:38:10 -0800

    How to measure code performance? use timeit module to measure time duration, profile and pstats provide identifying time critical sections in larger blocks of code

--- Python_Tutorial_10_stdlib.py
+++ Python_Tutorial_10_stdlib.py
@@ -128,3 +128,10 @@ t = zlib.compress(s)
 len(t)
 print(zlib.decompress(t))
 print(zlib.crc32(s))
+
+#Performance Measurement
+#use timeit module demonstrates a modest performance advantage
+from timeit import Timer
+print(Timer('t=a; a=b; b=t', 'a=1; b=2').timeit())
+print(Timer('a,b = b,a', 'a=1;b=2').timeit())
+
