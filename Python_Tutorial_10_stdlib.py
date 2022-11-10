38b0f353aef1ab814c0f99a515066588c180e42b
Lina-Liuna <liuna.lina@gmail.com>
2022-11-10 06:49:01 -0800

    Quaility Control: use doctest module to scanning a module and validating test; use unitest module for more comprehensive set of tests

--- Python_Tutorial_10_stdlib.py
+++ Python_Tutorial_10_stdlib.py
@@ -135,3 +135,27 @@ from timeit import Timer
 print(Timer('t=a; a=b; b=t', 'a=1; b=2').timeit())
 print(Timer('a,b = b,a', 'a=1;b=2').timeit())
 
+
+#Quality Control: write tests for each function is one approach for developing high quality software.
+#use doctest module to scanning a module and validating tests embedded in a program's docstrings.
+def average(values):
+    return sum(values) / len(values)
+
+import doctest
+average([10, 20, 40])
+doctest.testmod()
+
+#use unitest module for more comprehensive set of tests
+import unittest
+class TestStatisticalFunctions(unittest.TestCase):
+
+    def test_average(self):
+        self.assertEqual(average([20, 30, 70]), 40.0)
+        self.assertEqual(round(average([1,5,7]), 1), 4.3)
+        with self.assertRaises(ZeroDivisionError):
+            average([])
+        with self.assertRaises(TypeError):
+            average(20, 30, 70)
+
+unittest.main()
+
