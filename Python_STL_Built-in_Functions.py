e81b267282353e6baebdf360e348241c3fd9af79
Lina-Liuna <liuna.lina@gmail.com>
2022-11-16 07:52:55 -0800

    Add dir() enuerate() example

--- Python_STL_Built-in_Functions.py
+++ Python_STL_Built-in_Functions.py
@@ -4,3 +4,32 @@ print(bin(-11))
 
 print(format(11, '#b'))
 print(format(11, 'b'))
+
+# dir()
+import struct
+print(dir())
+print(dir(struct))
+
+class Shape:
+    def __dir__(self):
+        return ['area', 'perimeter', 'location']
+
+s = Shape()
+print(dir(s))
+
+
+seasons = ['spring', 'summer', 'fall', 'winter']
+print(list(enumerate(seasons)))
+print(list(enumerate(seasons, start=1)))
+
+
+def enumerate_new(sequences, start=0):
+    n = start
+    for elem in sequences:
+        yield n, elem
+        n += 1
+
+print(list(enumerate_new(seasons)))
+
+
+
