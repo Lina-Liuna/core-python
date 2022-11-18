1e1198d009ee6eb80df598ae2ffca6f7d5deb438
Lina-Liuna <liuna.lina@gmail.com>
2022-11-18 06:36:55 -0800

    WOW, amazing examples: reference instead of copy for the common sequence

--- Python_STL_Built-In_Types.py
+++ Python_STL_Built-In_Types.py
@@ -13,3 +13,16 @@ def bit_length(self):
     s = s.lstrip('-0b')
     return len(s)
 
+# 'in' and 'not in' operations used as subsequence testing
+print('app' in 'apple')
+
+# items in the sequence s are not copied; they are referenced multiple times
+# Wow, amazing reference instead of copy examples for the sequence.
+lists = [] * 3
+print(lists)
+
+listss = [[]] * 3
+print(listss)
+print(listss[0])
+listss[0].append(3)
+print(listss)
