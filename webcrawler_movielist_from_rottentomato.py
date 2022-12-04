5d597aee15d80c0396f3c6f6decbec4a08eadc05
Lina <liuna.lina@gmail.com>
2022-12-04 15:32:38 -0800

    web crawler movielist from rotten tomatoes

---
+++ webcrawler_movielist_from_rottentomato.py
@@ -0,0 +1,40 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+
+url = "https://www.rottentomatoes.com/top/bestofrt/"
+dir = '/Users/linaliu/Movies/rottentomatoes/'
+
+def get_movielist(url, dir):
+    f = requests.get(url)
+    soup = BeautifulSoup(f.content, 'lxml')
+    movies = soup.find_all('img')
+    print(movies)
+    for movie in movies:
+        if not movie.has_attr('src'):
+            continue
+        print(movie['src'])
+        print(movie['alt'])
+        img_url = movie['src']
+        movie_name = movie['alt']
+        pic_ext = '.' + img_url.split(".")[-1][0:3]
+        if 'https' not in img_url:
+            continue
+        if '.cn' in img_url:
+            continue
+        if '<em>' in movie_name:
+            print('hooked')
+            movie_name = movie_name.replace('<em>', '')
+        if '</em>' in movie_name:
+            print('hooked')
+            movie_name = movie_name.replace('</em>', '')
+        print(pic_ext)
+        img_data = requests.get(img_url).content
+        img_name = dir + movie_name + pic_ext
+        with open(img_name, 'wb') as handler:
+            handler.write(img_data)
+
+
+get_movielist(url, dir)
+
