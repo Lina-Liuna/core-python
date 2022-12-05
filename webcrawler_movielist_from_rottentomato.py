203d03f94ecec1abf9944cf501552ada1842227a
Lina <liuna.lina@gmail.com>
2022-12-04 17:06:46 -0800

    write the crawled data movie lists inforamtion to excel

--- webcrawler_movielist_from_rottentomato.py
+++ webcrawler_movielist_from_rottentomato.py
@@ -1,7 +1,11 @@
+from typing import Any
+
 import requests
 import lxml
 from bs4 import BeautifulSoup
-
+from bs4.element import ResultSet
+from xlwt import *
+from openpyxl import load_workbook
 
 url = "https://www.rottentomatoes.com/top/bestofrt/"
 dir = '/Users/linaliu/Movies/rottentomatoes/'
@@ -11,10 +15,13 @@ def get_movielist(url, dir):
     soup = BeautifulSoup(f.content, 'lxml')
     movies = soup.find_all('img')
     print(movies)
+    movie_lists = [[], []]
     for movie in movies:
         if not movie.has_attr('src'):
             continue
         print(movie['src'])
+        movie_lists[0].append(movie['src'])
+        movie_lists[1].append(movie['alt'])
         print(movie['alt'])
         img_url = movie['src']
         movie_name = movie['alt']
@@ -30,7 +37,24 @@ def get_movielist(url, dir):
         img_name = dir + movie_name + pic_ext
         with open(img_name, 'wb') as handler:
             handler.write(img_data)
+    return movie_lists
+
+def write_movieinfo_to_excel(dir, movie_info):
+    workbook = Workbook(encoding= 'utf-8')
+    table = workbook.add_sheet('data')
+    table.write(0, 0, 'Number')
+    table.write(0, 1, 'movie_url')
+    table.write(0, 2, 'movie_name')
+
+    for i in range(1, len(movie_info[0])):
+        table.write(i, 0, i)
+        table.write(i, 1, movie_info[0][i])
+        table.write(i, 2, movie_info[1][i])
 
 
-get_movielist(url, dir)
+    workbook.save(dir + 'movielist.xls')
+
+Movieinfo = get_movielist(url, dir)
+
+write_movieinfo_to_excel(dir, Movieinfo)
 
