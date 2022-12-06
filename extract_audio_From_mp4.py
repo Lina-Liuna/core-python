462e5f131283db7f3b588161ab190a774c1e63f3
Lina <liuna.lina@gmail.com>
2022-12-06 15:17:15 -0800

    extract audio from mp4 file

---
+++ extract_audio_From_mp4.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+from pathlib import Path
+
+video_path = '/Users/linaliu/Movies/youtube/temp/'
+file = "game"
+
+
+# command = "ffmpeg -i /Users/kc/books/librarybooks/audlt/audio/The_Subtle_art_of_not_giving_a_fuck.MP4 -ab 160k -ac 2 -ar 44100 -vn /Users/kc/books/librarybooks/audlt/audio/The_Subtle_art_of_not_giving_a_fuck.wav"
+
+
+def extract_audio(dir_path, filename):
+    command = "ffmpeg -i " + dir_path + filename + " -ab 160k -ac 2 -ar 44100 -vn " + dir_path + file + ".wav"
+    subprocess.call(command, shell=True)
+
+def extract_audios(dir):
+    os.chdir(dir)
+    #paths = sorted(Path(dir).iterdir(), key=os.path.getmtime)
+    paths = sorted(Path(dir).iterdir())
+    print(paths)
+    for myfile in paths:
+        basename = os.path.basename(myfile)
+        dirname = os.path.dirname(myfile)
+        if 'DS_Store' in basename:
+            continue
+        if 'audio' in basename:
+            continue
+        print(basename)
+        extract_audio(dir, basename)
+
+
+extract_audios(video_path)
