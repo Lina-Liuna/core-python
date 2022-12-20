0a4b8863c34b2da7c3c5a3afdea2b48385d4429f
Lina <liuna.lina@gmail.com>
2022-12-20 05:24:24 -0800

    Add re example file

---
+++ STL_Examples/re_regular_expression_operations.py
@@ -0,0 +1,18 @@
+# re - regular expression operations
+import re
+
+m = re.search('(?<=abc)def', 'abcdef')
+print(m.group())
+
+m = re.search('(?<=c)def', 'abcdef')
+print(m.group())
+
+m = re.search('(?<=bc)def', 'abcdef')
+print(m.group())
+
+m = re.search('(?<=)def', 'abcdef')
+print(m.group())
+
+m = re.search(r'(?<=-)\w+', 'spam-egg')
+print(m.group())
+
