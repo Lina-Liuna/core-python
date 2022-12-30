dc7826e5d40304457b4f1a6919a635737e39d13b
Lina <liuna.lina@gmail.com>
2022-12-30 07:53:14 -0800

    use sys.stdout.writelines(delta) function to specify diff format headers by adding filenames and modification times

--- STL_Examples/difflib-Helpers_for_computing_deltas.py
+++ STL_Examples/difflib-Helpers_for_computing_deltas.py
@@ -2,6 +2,7 @@
 # compare a and b(strings) return a delta(a generator generating the delta lines) in context diff format
 # returns a delta(a generator generating the delta lines)
 import difflib
+import sys
 
 s1 = ['cat\n', 'best\n', 'man\n', 'Good Morning!\n']
 s2 = ['mat\n', 'west\n', 'plan\n', 'Good Morning!\n']
@@ -17,3 +18,7 @@ diff = difflib.context_diff(s1, s2, n=0)
 print(diff)
 delta = ''.join(diff)
 print(delta)
+
+# The context diff format normally has a header for filenames and modification times.
+# Any or all of these may be specified using strings for fromfile, tofile, fromfiledate, and tofiledate
+sys.stdout.writelines(difflib.context_diff(s1, s2, fromfile='before.py', tofile='after.py', fromfiledate='Dec. 30 2022', tofiledate='Dec. 30, 2022'))
