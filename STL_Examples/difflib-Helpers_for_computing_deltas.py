aaba983e252c92013791072884cbc4b3f547cf07
Lina <liuna.lina@gmail.com>
2022-12-31 07:22:55 -0800

    add difflib.get_close_matches() exmaple, to get the similar words list, find similar keywords

--- STL_Examples/difflib-Helpers_for_computing_deltas.py
+++ STL_Examples/difflib-Helpers_for_computing_deltas.py
@@ -22,3 +22,17 @@ print(delta)
 # The context diff format normally has a header for filenames and modification times.
 # Any or all of these may be specified using strings for fromfile, tofile, fromfiledate, and tofiledate
 sys.stdout.writelines(difflib.context_diff(s1, s2, fromfile='before.py', tofile='after.py', fromfiledate='Dec. 30 2022', tofiledate='Dec. 30, 2022'))
+
+# difflib.get_close_matches
+# parameters: (words, possibilities, n=3, cutoff=0.6), words: a sequence for which close matches are desired
+# possibilities is a list of sequences against which to match word
+# return a list of the best ‘good enough’ matches.
+
+list = difflib.get_close_matches('belive', ['live', 'believe', 'achieve', 'inaprpropriate'])
+print(list)
+
+import keyword
+list = difflib.get_close_matches('else', keyword.kwlist)
+print(list)
+list = difflib.get_close_matches('el', keyword.kwlist)
+print(list)
