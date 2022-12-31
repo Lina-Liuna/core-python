e72dfd4b9789b75e13709a83e7943a966940cc2b
Lina <liuna.lina@gmail.com>
2022-12-31 14:23:09 -0800

    teeny-tiny remove any common leading whitespace from everyline in text

--- STL_Examples/textwrap_text_wrapping_and_filling.py
+++ STL_Examples/textwrap_text_wrapping_and_filling.py
@@ -4,3 +4,13 @@ s = 'It is raining outside, today is last day of 2022!'
 print(textwrap.shorten(s, width=len(s)))
 print(textwrap.shorten(s, width=len(s) - 5))
 print(textwrap.shorten(s, width=len(s) - 5, placeholder='***'))
+
+# remove any common leading whitespace from every line in text
+s = """\
+       she told me she likes me as a friend.
+           I told her that in C++, Friends can 
+              access the private parts
+"""
+print(s)
+print(repr(s))
+print(repr(textwrap.dedent(s)))
