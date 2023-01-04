b39849ddf90a434469110e7bd4c021bcfddae380
Lina <liuna.lina@gmail.com>
2023-01-04 06:34:07 -0800

    bisect.bisect combined with namedtuple key not worked???????

--- STL_Examples/bisect_Array_Bisection_algorithm.py
+++ STL_Examples/bisect_Array_Bisection_algorithm.py
@@ -18,6 +18,7 @@
 # example: uses bisect() to look up a letter grade for an exam score based on a set of ordered numeric breakpoints:
 # 90 and up is 'A', '80 to 89 is B'
 import bisect
+import collections
 
 
 def grade(score, breakpoints=[60,70,80,90], grades='FDCBA'):
@@ -27,3 +28,26 @@ def grade(score, breakpoints=[60,70,80,90], grades='FDCBA'):
 scores = [55,66,77,88,99]
 grades = [grade(score) for score in scores]
 print(grades)
+
+# teeny-tiny-useful:
+# bisect() and insort() func work with lists of tuples.
+# The key argument can serve to extract the field used for ordering records in a table.
+import collections
+import bisect
+import operator
+import pprint
+Movie = collections.namedtuple('Movie', ('name', 'released', 'director'))
+movies = [
+    Movie('schindlers List', 1993, 'Steven Spielberg'),
+    Movie('Taxi Driver', 1996,'Martin Scorsese'),
+    Movie('The Martian',2015,'Ridley Scott'),
+    Movie('Ying hung boon sik',1986,'John Woo'),
+]
+
+# Find first movie released after 1980
+by_year = operator.attrgetter('released')
+movies.sort(key=by_year)
+print(movies)
+#help(bisect.bisect)
+#m = movies[bisect.bisect_left(movies, 1988, key=by_year)]
+
