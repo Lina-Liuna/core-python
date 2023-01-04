483d8dcecc15c01d58fa188525cc490d00b00b9a
Lina <liuna.lina@gmail.com>
2023-01-04 07:02:14 -0800

    IMPORTANT: highly discourage option but useful way fo fix: urllib and SSL: CERTIFICATE_VERIFY_FAILED  Error

--- STL_Examples/pprint_data_pretty_printer.py
+++ STL_Examples/pprint_data_pretty_printer.py
@@ -7,3 +7,20 @@ pp.pprint(stuff)
 
 pp = pprint.PrettyPrinter(width=61, compact=True)
 pp.pprint(stuff)
+
+
+import json
+import pprint
+import urllib
+from urllib.request import urlopen
+# This restores the same behavior as before.
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+#input = input.replace("!web ", "")
+url = 'https://pypi.org/pypi/sampleproject/json'
+with urlopen(url) as resp:
+    project_info = json.load(resp)
+
+pprint.pprint(project_info)
