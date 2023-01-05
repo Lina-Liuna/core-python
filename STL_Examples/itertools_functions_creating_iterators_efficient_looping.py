8b17e304c66133a1d8ca58e9c08faa312a1a9c4e
Lina <liuna.lina@gmail.com>
2023-01-05 12:33:39 -0800

    Add print result comments

--- STL_Examples/itertools_functions_creating_iterators_efficient_looping.py
+++ STL_Examples/itertools_functions_creating_iterators_efficient_looping.py
@@ -10,17 +10,22 @@ import operator
 data = [1,2,3,4,5,6,7,8,9,0,9,8,7,6]
 result = itertools.accumulate(data, operator.mul)
 print(list(result))
+# [1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 0, 0, 0, 0, 0]
 
 result = itertools.accumulate(data, operator.add)
 print(list(result))
+# [1, 3, 6, 10, 15, 21, 28, 36, 45, 45, 54, 62, 69, 75]
 
 result = itertools.accumulate(data, max)
 print(list(result))
+#[1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 9]
 
 # Amortize a 5% loan of 1000 with 4 annual payments of 90
 cashflows = [1000, -90, -90, -90, -90]
 result = itertools.accumulate(cashflows, lambda bal, pmt: bal*1.05 + pmt)
 print(list(result))
+# [1000, 960.0, 918.0, 873.9000000000001, 827.5950000000001]
+
 
 # itertools.repeat()
 # Make an iterator that returns object over and over again.
@@ -28,6 +33,8 @@ print(list(result))
 # A common use for repeat is to supply a stream of constant values to map or zip:
 result = map(pow, range(10), itertools.repeat(2))
 print(list(result))
+# [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
 result = map(pow, range(10), itertools.repeat(3))
 print(list(result))
+# [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
