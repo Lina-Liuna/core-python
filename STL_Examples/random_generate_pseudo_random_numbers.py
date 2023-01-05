e7e8c87604f201514c3969523853d0c4e213c638
Lina <liuna.lina@gmail.com>
2023-01-05 06:50:43 -0800

    Tiny-useful on medicine: staticstics combined random to resampling permutation test effect of a drug versus a placebo

--- STL_Examples/random_generate_pseudo_random_numbers.py
+++ STL_Examples/random_generate_pseudo_random_numbers.py
@@ -73,6 +73,8 @@ print(sum(trial() for i in range(10_000)) / 10_000)
 
 # Example of statistical bootstrapping using resampling with replacement to estimate a
 # confidence interval for the mean of a sample
+# https://www.thoughtco.com/example-of-bootstrapping-3126155
+# https://en.wikipedia.org/wiki/Bootstrapping_(statistics)
 from statistics import fmean as mean
 data = [11,22,33,44,55,66,77,88,99,00, 12,13,14,15, 16]
 random_choice = random.choices(data, k=len(data))
@@ -83,3 +85,32 @@ print(mean_rc)
 means = sorted(mean(random.choices(data, k=len(data))) for i in range(100))
 print(f'The sample mean of {mean(data):.1f} has a 90% confidence '
       f'interval from {means[5]:.1f} to {means[94]:.1f}')
+
+# Examples of a resampling permutation test to determine the statistical significance or p-value
+# of an observed difference between the effects of a drug vs a placebo:
+# Example from "Statistics is Easy" by Dennis Shasha and Manda Wilson
+
+import statistics
+
+print(help(statistics.fmean))
+# fmean(data): convert data to floats and  compute the arithmetic mean.
+drug = [54, 73, 53, 70, 73, 68, 52, 65, 65]
+placebo = [54, 51, 58, 44, 55, 52, 42, 47, 58, 46]
+observed_diff = statistics.fmean(drug) - statistics.fmean(placebo)
+
+print(help(random.shuffle))
+n = 10_000
+count = 0
+combined = drug + placebo
+random.shuffle(combined)
+print(combined)
+for i in range(n):
+    random.shuffle(combined)
+    new_diff = statistics.fmean(combined[:len(drug)]) - statistics.fmean(combined[len(drug):])
+    count += (new_diff >= observed_diff)
+
+print(f'{n} label reshufflings produced only {count} instances with a difference')
+print(f'at least as extreme as the observed difference of {observed_diff:.1f}.')
+print(f'The one-sided p-value of {count / n:.4f} leads us to reject the null')
+print(f'hypothesis that there is no difference between the drug and the placebo.')
+
