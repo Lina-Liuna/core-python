e1c8a85d774cdfcdbddf6de85bfb7592b6a2e90f
Lina <liuna.lina@gmail.com>
2023-01-05 06:56:54 -0800

    tiny-useful on simulation: statistics combined random to simulation of arrival times and service deliveries for a multiserver queue

--- STL_Examples/random_generate_pseudo_random_numbers.py
+++ STL_Examples/random_generate_pseudo_random_numbers.py
@@ -114,3 +114,32 @@ print(f'at least as extreme as the observed difference of {observed_diff:.1f}.')
 print(f'The one-sided p-value of {count / n:.4f} leads us to reject the null')
 print(f'hypothesis that there is no difference between the drug and the placebo.')
 
+# Simulation of arrival times and service deliveries for a multiserver queue:
+from heapq import heapify, heapreplace
+from random import expovariate, gauss
+from statistics import mean, quantiles
+
+average_arrival_interval = 5.6
+average_service_time = 15.0
+stdev_service_time = 3.5
+num_servers = 3
+
+waits = []
+arrival_time = 0.0
+servers = [0.0] * num_servers  # time when each server becomes available
+heapify(servers)
+for i in range(1_000_000):
+    arrival_time += expovariate(1.0 / average_arrival_interval)
+    next_server_available = servers[0]
+    wait = max(0.0, next_server_available - arrival_time)
+    waits.append(wait)
+    service_duration = max(0.0, gauss(average_service_time, stdev_service_time))
+    service_completed = arrival_time + wait + service_duration
+    heapreplace(servers, service_completed)
+
+print(f'Mean wait: {mean(waits):.1f}   Max wait: {max(waits):.1f}')
+print('Quartiles:', [round(q, 1) for q in quantiles(waits)])
+
+# https://www.youtube.com/watch?v=Iq9DzN6mvYA
+
+
