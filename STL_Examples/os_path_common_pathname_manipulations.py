01b0eac82e0183363de5f62332e0b5f21b36d685
Lina <liuna.lina@gmail.com>
2023-01-07 08:47:54 -0800

    examples: split file through . by using os.path.splittext

--- STL_Examples/os_path_common_pathname_manipulations.py
+++ STL_Examples/os_path_common_pathname_manipulations.py
@@ -9,9 +9,12 @@ print(common_prefix)
 common_prefix = os.path.commonpath(['/usr/lib', '/usr/local/lib'])
 print(common_prefix)
 
-p = pathlib.Path('~/Booklist')
+p = pathlib.Path('~/Booklist/README.md')
 p = p.expanduser()
 
 split_path = os.path.splitdrive(str(p))
 print(split_path)
+
+split_text = os.path.splitext(str(p))
+print(split_text)
 
