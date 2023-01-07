a4202135adb1f7365f48d2a1151e7af1964697d1
Lina <liuna.lina@gmail.com>
2023-01-07 08:45:37 -0800

    tiny-useful: splitdrive by using os.path.splitdrive

--- STL_Examples/os_path_common_pathname_manipulations.py
+++ STL_Examples/os_path_common_pathname_manipulations.py
@@ -1,9 +1,17 @@
 # os.path.commonprefix(list)
 # return the longest path prefix(taken character-by-character) that is a prefix of all paths in list
 import os.path
+import pathlib
 
 common_prefix = os.path.commonprefix(['/usr/lib', '/usr/local/lib'])
 print(common_prefix)
 
 common_prefix = os.path.commonpath(['/usr/lib', '/usr/local/lib'])
 print(common_prefix)
+
+p = pathlib.Path('~/Booklist')
+p = p.expanduser()
+
+split_path = os.path.splitdrive(str(p))
+print(split_path)
+
