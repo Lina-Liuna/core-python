8d751147330021d4cf45efcf2de0bce0ab6acdbd
Lina <liuna.lina@gmail.com>
2023-01-08 06:47:34 -0800

    glob.glob() find all files in current directory matching a certain pattern

---
+++ STL_Examples/glob_unix_style_pathname_pattern_expansion.py
@@ -0,0 +1,10 @@
+# the blob find all the pathnames matching a specified pattern
+
+import glob
+
+l = glob.glob('[0-9].*')
+print(l)
+
+# output all files under current directory
+l = glob.glob('*.*')
+print(l)
