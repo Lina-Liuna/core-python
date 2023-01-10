3bf4ee5f175620ee50142c523978c2fe91c17680
Lina <liuna.lina@gmail.com>
2023-01-10 06:41:01 -0800

    Shelve module: dictionary-like object. values in a shelve can be arbitrary python object which pickle can handle

---
+++ STL_Examples/shelve_python_object_persistence.py
@@ -0,0 +1,4 @@
+# A shelf is a persistent, dictionary-like object.
+# The difference with dbm databases is that the values in a shelf can be essentially arbitrary python objects
+# anything that the pickle module can handle.
+# the keys are ordinary strings
