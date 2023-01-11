e95d6841852f4382eac09e52a7726d54d5d331aa
Lina <liuna.lina@gmail.com>
2023-01-11 05:05:12 -0800

    what the urllib.parse do? 1. split URL string into its components; 2. combining URL components into a URL string

--- Effective_Python/ch1_item5_write_helper_func_instead_complex_expressions.py
+++ Effective_Python/ch1_item5_write_helper_func_instead_complex_expressions.py
@@ -1,3 +1,19 @@
+# urllib.parse used to break URL(Uniform Resource Locator) strings up in components(addressing scheme,
+# network location, path etc.)
+#
+# urllib.parse used to combine the components back into a URL string,
+#
+# urllib.parse used to convert a relative URL to an absolute URL given a base URL
+
+# urllib.parse support URL schemes: file, ftp, gopher,
+# hdl, http, https, imap, mailto, mms, news, nntp, prospero, rsync, rtsp, rtspu, sftp, shttp, sip, sips, snews, svn,
+# svn+ssh, telnet, wais, ws, wss.
+
+# urllib.parse module defines functions that fall into two broad categories: url parsing and url quoting.
+
+# urllib.parse focus on splitting a URL string into its components,
+# urllib.parse focus on combining URL components into a URL string.
+
 from urllib.parse import parse_qs
 my_values = parse_qs('red=5&blue=0&green=',keep_blank_values=True)
 # repr() functions return a printable representation of the given object
