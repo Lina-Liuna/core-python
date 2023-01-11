a9539c60e560758cc173f258807c83dcdc54d885
Lina <liuna.lina@gmail.com>
2023-01-11 05:42:15 -0800

    useful example: dictionary.items(), convert to tuple

---
+++ Effective_Python/ch1_item6_prefer_multiple_assignment_unpacking_over_indexing.py
@@ -0,0 +1,23 @@
+# tuple used to create immutable, ordered sequence of values.
+# A tuple is a pair of two values, such as keys and values from a dictionary
+
+lina_food_calories = {
+    'kirkland oat milk': 100,
+    'vegetables': 30,
+    'berries': 50,
+    'nuts': 150,
+}
+# dict.items() return a view objects, return a new view of the dictionary's items(key, value) pairs
+# what's the view object?
+# The view object provide a dynamic view on the dictionary's entries, which means that when the dictionary
+# changes, the view reflects these changes.
+print(lina_food_calories.items())
+
+items = set(lina_food_calories.items())
+print(items)
+
+items = list(lina_food_calories.items())
+print(items)
+
+items = tuple(lina_food_calories.items())
+print(items)
