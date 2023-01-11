95a367ffac7367fee5fdedf36518c56173d2973a
Lina <liuna.lina@gmail.com>
2023-01-11 05:25:02 -0800

    write a helper function to reuse the logic repeatly

--- Effective_Python/ch1_item5_write_helper_func_instead_complex_expressions.py
+++ Effective_Python/ch1_item5_write_helper_func_instead_complex_expressions.py
@@ -39,4 +39,24 @@ print(f'red: {red!r}')
 print(f'green: {green!r}')
 print(f'opacity:{opacity!r}')
 
+# The above is a little hard to read. use if else keeping the code short
+red_str = my_values.get('red', [''])
+red = int(red_str[0]) if red_str[0] else 0
+
+# but the repeat of red green opacity made it more complicated.
+
+# solution: reuse the logic repeatly
+
+def get_first_int(values, key, default=0):
+    found = values.get(key, [''])
+    if found[0]:
+        return int(found[0])
+    return default
+
+red = get_first_int(my_values, 'red')
+green = get_first_int(my_values, 'green')
+opacity = get_first_int(my_values, 'opacity')
+print(red, green, opacity)
+
+
 
