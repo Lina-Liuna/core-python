806d24b60cf13d1b4602a97eed7e635a17e399d1
Lina <liuna.lina@gmail.com>
2023-01-12 12:52:24 -0800

    What is  sqlite used for? how to use Sqlite?

---
+++ STL_Examples/sqlite3_DB-API_2.0_interface_for_sqlite_databases.py
@@ -0,0 +1,5 @@
+# SQLite is a C library that provides a lightweight disk-based database that doesn't require a separate
+# server process.
+
+# SQLite allows accessing the database using a nonstandard variant of the SQL query language.
+
