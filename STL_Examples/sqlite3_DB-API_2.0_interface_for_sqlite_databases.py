02d298c1996905e7b28323667ce184d0ca7181ff
Lina <liuna.lina@gmail.com>
2023-01-12 12:56:45 -0800

    who will use SQLite3? 1. application use SQLite for internal data stroage, 2. prototype an application, later port code to larger database like PostgreSQL or Oracle

--- STL_Examples/sqlite3_DB-API_2.0_interface_for_sqlite_databases.py
+++ STL_Examples/sqlite3_DB-API_2.0_interface_for_sqlite_databases.py
@@ -3,3 +3,5 @@
 
 # SQLite allows accessing the database using a nonstandard variant of the SQL query language.
 
+# Some applications can use SQLite for internal data storage.
+# Can protoypte an application useing SQLite and then port the code to a larger database such as PostgreS@QL or Oracle.
