59fb6cf8f23908118e5bd0d3a92914acc1616e64
Lina <liuna.lina@gmail.com>
2023-01-13 05:47:57 -0800

    python enumerate func wraps any iterator with a lazy generator, you can use next() in returned lazy generator

--- Effective_Python/ch1_item7_prefer_enumerate_over_range.py
+++ Effective_Python/ch1_item7_prefer_enumerate_over_range.py
@@ -23,3 +23,9 @@ for flavor in flavor_list:
 # you'll iterate over a list by the index of the current item in the list.
 
 # but iterate over sequences or over the index looks clumsy
+# Built-in function enumerate wraps any iterator with a lazy generator.
+# enumerate yields pairs of the loop index and the next value from the given iterator.
+it = enumerate(flavor_list)
+print(next(it))
+print(next(it))
+
