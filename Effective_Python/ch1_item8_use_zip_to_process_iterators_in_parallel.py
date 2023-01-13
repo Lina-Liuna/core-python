bee4dfb6b132faf8d2985dafd7c8f60c24b8b295
Lina <liuna.lina@gmail.com>
2023-01-13 06:01:10 -0800

    problem: with many lists, how to handle many list parallel?

---
+++ Effective_Python/ch1_item8_use_zip_to_process_iterators_in_parallel.py
@@ -0,0 +1,4 @@
+# Problem:
+# Often in python you find yourself with many lists of related objects
+# list comprehensions make it easy to take a source list and get a derived list by applying an expression.
+
