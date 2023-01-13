5201fe22688c6c1091e98b893a782545a4205c29
Lina <liuna.lina@gmail.com>
2023-01-13 06:18:18 -0800

    tiny-useful example: find longest verb and its count by using zip

--- Effective_Python/ch1_item8_use_zip_to_process_iterators_in_parallel.py
+++ Effective_Python/ch1_item8_use_zip_to_process_iterators_in_parallel.py
@@ -2,3 +2,15 @@
 # Often in python you find yourself with many lists of related objects
 # list comprehensions make it easy to take a source list and get a derived list by applying an expression.
 
+phrasal_verbs = ['call off', 'bring up', 'come up', 'hand over', 'talk over', 'go over']
+counts = [len(n) for n in phrasal_verbs]
+
+# find longest phrasal verb in verb list:
+max_count = 0
+longest_name = str()
+for name, count in zip(phrasal_verbs, counts):
+    if count > max_count:
+        longest_name = name
+        max_count = count
+
+print(max_count, longest_name)
