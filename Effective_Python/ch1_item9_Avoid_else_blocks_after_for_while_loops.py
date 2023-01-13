06defd52db8d7de3f240fafa82b23f69ce41cba3
Lina <liuna.lina@gmail.com>
2023-01-13 06:37:30 -0800

    avoid using else blocks after loops use helper func instead

---
+++ Effective_Python/ch1_item9_Avoid_else_blocks_after_for_while_loops.py
@@ -0,0 +1,6 @@
+# write a helper function to avoid else blocks after for and while loops
+
+# break the loop as soon as find the target.
+
+# things to remember:
+# Avoid using else blocks after loops because their behavior isn't intuitive and can be confusing.
