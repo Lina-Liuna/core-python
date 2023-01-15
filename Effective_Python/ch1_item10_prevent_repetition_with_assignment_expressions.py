ee1127c7343ee32d00074073a0e24fa820a37aaf
Lina <liuna.lina@gmail.com>
2023-01-15 05:30:18 -0800

    New syntax after 3.8: Walrus operator: solve a long-standing problem that can cause code duplication

--- Effective_Python/ch1_item10_prevent_repetition_with_assignment_expressions.py
+++ Effective_Python/ch1_item10_prevent_repetition_with_assignment_expressions.py
@@ -1,3 +1,5 @@
 # a = b pronounced "a equals b"
 # a := b pronounced 'a walrus b' why? because := looks like a pair of eyeballs and tusks
 
+# walrus operator is a new syntax(3.8 or later)
+# walrus operator used to solve a long-standing problem with the language that can cause code duplication.
