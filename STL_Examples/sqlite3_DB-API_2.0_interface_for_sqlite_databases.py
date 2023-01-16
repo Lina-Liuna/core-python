4952bb6573d0505e1a7319fcea03675695b30745
Lina <liuna.lina@gmail.com>
2023-01-16 08:53:05 -0800

    con.create_collation example: reverse sorting collation

--- STL_Examples/sqlite3_DB-API_2.0_interface_for_sqlite_databases.py
+++ STL_Examples/sqlite3_DB-API_2.0_interface_for_sqlite_databases.py
@@ -124,4 +124,25 @@ print(res.fetchall())
 
 con.close()
 
+# example: shows a reverse sorting collation
+def collate_reverse(string1, string2):
+    if string1 == string2:
+        return 0
+    elif string1 < string2:
+        return 1
+    else:
+        return -1
+
+con = sqlite3.connect(":memory:")
+con.create_collation("reverse", collate_reverse)
+
+cur = con.execute("CREATE TABLE test(x)")
+cur.executemany("INSERT INTO test(x) VALUES(?)", [("a",), ("b",)])
+cur.execute("SELECT x FROM test ORDER BY x COLLATE reverse")
+for row in cur:
+    print(row)
+con.close()
+
+# copy 
+
 
