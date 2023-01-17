b5f15cedf384f6798452b5d7da483d3919b41fd6
Lina <liuna.lina@gmail.com>
2023-01-17 06:37:51 -0800

    useful-examples: fill a text into compression GZIP file

--- STL_Examples/gzip_support_for_gzip_files.py
+++ STL_Examples/gzip_support_for_gzip_files.py
@@ -4,11 +4,16 @@ import os.path
 import pathlib
 
 # use gzip to compress an existing file.
-filename ='README.md'
-p = pathlib.Path('~/code/Booklist/' + filename)
+filename ='temp.db'
+p = pathlib.Path('~/code/core-python/STL_Examples/' + filename)
 p = p.expanduser()
 print(p)
 print(str(p))
 with open(p, 'rb') as f_in:
     with gzip.open(str(p) +'.gz', 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
+
+# Create a compressed GZIP file
+content = b'Miss You'
+with gzip.open(p, 'wb') as f:
+    f.write(content)
