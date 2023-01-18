d42b7eca73cda50348565ecd691113366d0217b6
Lina <liuna.lina@gmail.com>
2023-01-18 06:42:44 -0800

    useful-example: kids favorites amusement park and museum

--- Effective_Python/ch2_item13_prefer_catch_all_unpacking_over_slicing.py
+++ Effective_Python/ch2_item13_prefer_catch_all_unpacking_over_slicing.py
@@ -15,3 +15,16 @@ print(oldest, youngest)
 
 # to unpacking assignment that contain a starred expression, you must have at least one required part, or error.
 # *others = car_age_descending # Error
+
+# you can't use multiple catch-all expressions in a single-level unpacking pattern:
+# first, *middle, *second_middle, last = xxxx # Error
+
+kids_play = {
+    'Amusement Park' : ('Great America', 'Gilroy Garden', 'Six Flags'),
+    'Museum' : ('Blackhawk Museum', 'MOMA', 'Kids Discovery')
+}
+
+((loc1, (best1, *rest1)),
+ (loc2, (best2, *rest2))) = kids_play.items()
+print(f'Best at {loc1} is {best1}, {len(rest1)} others')
+print(f'Best at {loc2} is {best2}, {len(rest2)} others')
