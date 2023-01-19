d3bea887a34f7841b6082d42540f097ae721ad6a
Lina <liuna.lina@gmail.com>
2023-01-19 08:16:05 -0800

    Note-Useful: Use the collections.OrderedDict(sorted(dict.items())) to sort by dict.keys

--- Effective_Python/ch2_item14_sort_by_complex_criteria_using_key_parameter.py
+++ Effective_Python/ch2_item14_sort_by_complex_criteria_using_key_parameter.py
@@ -9,6 +9,7 @@
 
 
 # What does Sort do with objects?
+import collections
 
 class English_Words:
     def __init__(self, name, meaning):
@@ -25,6 +26,20 @@ words_list = [
     English_Words('reliant', 'self-reliant'),
 ]
 
-print('Unsorted:', repr(words_list))
-words_list.sort(key=lambda x: x.name)
-print('\nSorted:  ', words_list)
+new_words = collections.defaultdict(list)
+new_words = {
+    'stride': ['walk with long, decisive steps.', 'he strode across the road'],
+    'collation': ['the action of collating something or a light informal meal', 'data management and collation, lunch '
+                                                                                'wwas a collation of salami, olives, '
+                                                                                'and rye bread'],
+    'trivial': ['of little value or importance', 'the story spends too much time on trivial matters'],
+    'reliant': ['dependent on someone or something', 'self-reliant'],
+    'oars': ['a pole with a flat blade, pivoting in an oar lock, used to row or steer a boat through the water.', ''],
+    'conviction': ['a firmly held belief or opinion',
+                   'He said he was enjoying his new job, but his voice lacked conviction'],
+}
+print('Unsorted:', repr(new_words))
+#list(new_words.items()).sort(key=lambda x: x.keys)
+#res = sorted(new_words_lists, key=lambda x: x[k])
+res = collections.OrderedDict(sorted(new_words.items()))
+print('\nSorted:  ', res.keys())
