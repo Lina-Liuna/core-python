b24cb3c27fccf1956a5778d1685d9204566b5203
Lina <liuna.lina@gmail.com>
2023-01-19 05:32:51 -0800

    Add ch2 item14: Sort by complex criteria using key parameter

---
+++ Effective_Python/ch2_item14_sort_by_complex_criteria_using_key_parameter.py
@@ -0,0 +1,13 @@
+# List Sort method
+
+# The list built-in type provides a sort method for ordering the items in a list instance based on a
+# variety of criteria.
+
+# By default, sort will order a list's contents by the natural ascending order of the items.
+
+# The sort method works for nearly all built-in types(strings, floats, etc.) that have a natural ordering to them.
+
+
+# What does Sort do with objects?
+
+
