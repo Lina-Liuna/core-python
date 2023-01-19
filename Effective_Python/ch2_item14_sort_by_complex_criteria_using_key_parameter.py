7e6ebebb44012f27a9520d33a8d4f9dcffb10028
Lina <liuna.lina@gmail.com>
2023-01-19 06:45:26 -0800

    very-useful: Sort new words list by using the lambda keywords to define a funciton for the key parameter

--- Effective_Python/ch2_item14_sort_by_complex_criteria_using_key_parameter.py
+++ Effective_Python/ch2_item14_sort_by_complex_criteria_using_key_parameter.py
@@ -10,4 +10,21 @@
 
 # What does Sort do with objects?
 
+class English_Words:
+    def __init__(self, name, meaning):
+        self.name = name
+        self.meaning = meaning
 
+    def __repr__(self):
+        return f'English_Words({self.name!r}: {self.meaning})'
+
+words_list = [
+    English_Words('stride', 'walk with long, decisive steps.'),
+    English_Words('collation', 'the action of collating something or a light informal meal'),
+    English_Words('trivial', 'of little value or importance'),
+    English_Words('reliant', 'self-reliant'),
+]
+
+print('Unsorted:', repr(words_list))
+words_list.sort(key=lambda x: x.name)
+print('\nSorted:  ', words_list)
