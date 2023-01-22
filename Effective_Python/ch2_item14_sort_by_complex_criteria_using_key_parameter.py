e649b0ae694d3c105fe53e131ee8944550cea66c
Lina <liuna.lina@gmail.com>
2023-01-22 05:47:36 -0800

    very-useful: tools sorts example: use lambda keywords to define a function for the key parameter

--- Effective_Python/ch2_item14_sort_by_complex_criteria_using_key_parameter.py
+++ Effective_Python/ch2_item14_sort_by_complex_criteria_using_key_parameter.py
@@ -7,25 +7,8 @@
 
 # The sort method works for nearly all built-in types(strings, floats, etc.) that have a natural ordering to them.
 
-
-# What does Sort do with objects?
+# How to sort a dictionary by keys?
 import collections
-
-class English_Words:
-    def __init__(self, name, meaning):
-        self.name = name
-        self.meaning = meaning
-
-    def __repr__(self):
-        return f'English_Words({self.name!r}: {self.meaning})'
-
-words_list = [
-    English_Words('stride', 'walk with long, decisive steps.'),
-    English_Words('collation', 'the action of collating something or a light informal meal'),
-    English_Words('trivial', 'of little value or importance'),
-    English_Words('reliant', 'self-reliant'),
-]
-
 new_words = collections.defaultdict(list)
 new_words = {
     'stride': ['walk with long, decisive steps.', 'he strode across the road'],
@@ -43,3 +26,27 @@ print('Unsorted:', repr(new_words))
 #res = sorted(new_words_lists, key=lambda x: x[k])
 res = collections.OrderedDict(sorted(new_words.items()))
 print('\nSorted:  ', res.keys())
+
+
+# What does Sort do with objects?
+
+class Tool:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def __repr__(self):        # __repr__() func returns the object representation in string format, can be used to reconstruct the object
+        return f'Tool({self.name!r}, {self.weight})'
+
+
+tools = [
+    Tool('level', 3.5),
+    Tool('hammer', 1.25),
+    Tool('screwdriver', 0.5),
+    Tool('chisel', 0.25)
+]
+
+# use the lambda keywords to define a function for the key parrameter that enables me to sort the list of Tool objects
+print('Unsorted:', repr(tools))
+tools.sort(key=lambda x: x.name)
+print('\nSorted:', tools)
