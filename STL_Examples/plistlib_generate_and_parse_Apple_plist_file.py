485eb303c37c2a02b5d1ee28987303e9425588aa
Lina <liuna.lina@gmail.com>
2023-01-25 06:29:50 -0800

    new-module: plistlib: Generate and Parse Apple .plist files

---
+++ STL_Examples/plistlib_generate_and_parse_Apple_plist_file.py
@@ -0,0 +1,8 @@
+# plist module: provide an interface for reading and writing the "property list" files used by Apple.
+# This module supports both binary and XML plist files.
+
+# The property list(.plist) file format is a simple serialization supporting basic object types, like dictionaries,
+# lists, numbers and strings.
+
+# To write out and parse a plist file, use the dump() and load() functions
+# To work with plist data in bytes objects, use dumps() and loads() functions
