d65a244fc14a73c2849c62e89112337082777ca3
Lina <liuna.lina@gmail.com>
2023-01-25 06:11:22 -0800

    Not-ideal-result-example: get votes ranks: by define a new dictionary-like class

--- Effective_Python/ch2_item15_cautious_when_relying_on_dict_insertion_ordering.py
+++ Effective_Python/ch2_item15_cautious_when_relying_on_dict_insertion_ordering.py
@@ -61,3 +61,45 @@ print(sorted_ranks.data)
 winner = get_winner(sorted_ranks)
 print(winner)
 
+#
+from typing import Dict, MutableMapping
+
+
+def populate_ranks_2(votes: Dict[str, int],
+                   ranks: Dict[str, int]) -> None:
+    names = list(votes.keys())
+    names.sort(key=votes.get, reverse=True)
+    for i, name in enumerate(names, 1):
+        ranks[name] = i
+
+
+def get_winner_2(ranks:Dict[str, int]) -> str:
+    return next(iter(ranks))
+
+class SortedDict2(MutableMapping[str, int]):
+    def __init__(self):
+        self.data = {}
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
+
+    def __iter__(self):
+        keys = list(self.data.keys())
+        keys.sort()
+        for key in keys:
+            yield key
+
+    def __len__(self):
+        return len(self.data)
+
+sorted_ranks_2 = SortedDict2()
+populate_ranks_2(votes, sorted_ranks_2)
+print(sorted_ranks_2.data)
+winner = get_winner_2(sorted_ranks_2)
+print(winner)
