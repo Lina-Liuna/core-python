8696cad2463e8dddb76bcd49231bdf97a073c41b
Lina <liuna.lina@gmail.com>
2023-01-25 06:02:03 -0800

    problem-exist-example: get votes ranks by define a new dictionary-like class that iterate its contents in alphabetical order

--- Effective_Python/ch2_item15_cautious_when_relying_on_dict_insertion_ordering.py
+++ Effective_Python/ch2_item15_cautious_when_relying_on_dict_insertion_ordering.py
@@ -25,3 +25,39 @@ def get_winner(ranks):
     return next(iter(ranks))
 print(get_winner(ranks))
 
+
+# rank the votes in alphabetical order instead of rand order
+#
+# use the collections.abc built-in module to define a new
+# dictionary-like class that iterates its contents in alphabetical order.
+
+from collections.abc import MutableMapping
+
+class SortedDict(MutableMapping):
+    def __init__(self):
+        self.data = {}
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
+
+    def __iter__(self):
+        keys = list(self.data.keys())
+        keys.sort()
+        for key in keys:
+            yield key
+
+    def __len__(self):
+        return len(self.data)
+
+sorted_ranks = SortedDict()
+populate_ranks(votes, sorted_ranks)
+print(sorted_ranks.data)
+winner = get_winner(sorted_ranks)
+print(winner)
+
