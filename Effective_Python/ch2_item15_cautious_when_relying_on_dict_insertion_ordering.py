89e7d6e8fccf9c38cae4ab9e42db2ef0f2e6f76a
Lina <liuna.lina@gmail.com>
2023-01-25 05:46:49 -0800

    useful-example: rank US presidential elections votes

---
+++ Effective_Python/ch2_item15_cautious_when_relying_on_dict_insertion_ordering.py
@@ -0,0 +1,22 @@
+# NOTE about collections.OrderedDict:
+# If you need to handle a high rate of key insertions and popitem calls, OrderedDict may be a better fit
+# You shouldn't always assume that insertion ordering behavior will be present when you're handling dictionaries.
+# Python makes it easy for programmers to define their own custom container types
+
+votes = {
+    'Joe Biden': 306,
+    'Trump': 304,
+    'Obama':365,
+    'Bush': 286,
+    'Clinton': 379,
+}
+
+def populate_ranks(votes, ranks):
+    names = list(votes.keys())
+    names.sort(key=votes.get, reverse=True)
+    for i, name in enumerate(names, 1):
+        ranks[name] = i
+
+ranks = dict()
+populate_ranks(votes, ranks)
+print(ranks)
