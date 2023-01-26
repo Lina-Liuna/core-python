bd89becd93570eaa74a417a7dbcac4a3b6fdf97f
Lina <liuna.lina@gmail.com>
2023-01-26 05:37:33 -0800

    Add-hash-function: How to hash a string to a digest/hexdigest

--- STL_Examples/hashlib_secure_hashes_and_message_digests.py
+++ STL_Examples/hashlib_secure_hashes_and_message_digests.py
@@ -1,3 +1,19 @@
 # hashlib module implements a common interface to many different secure hash and message digest algorithms.
 
 # included FIPS secure hash algorithms SHA1, SHA224, SHA256, SHA384 and SHA512 as well as RSA's MD5 algorithm.
+import hashlib
+
+def get_hash_str_digest(str):
+    m = hashlib.sha256()
+    m.update(str)
+    return m.digest()
+
+def get_hash_str_hex_digest(str):
+    m = hashlib.sha256()
+    m.update(str)
+    return m.hexdigest()
+
+str = b"Fortunate Perhaps Infantile"
+print(get_hash_str_hex_digest(str))
+print(get_hash_str_digest(str))
+
