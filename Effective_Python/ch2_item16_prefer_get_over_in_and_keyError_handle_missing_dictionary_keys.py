0458b9f381376e8565bd99755ecfc7b329499911
Lina <liuna.lina@gmail.com>
2023-01-26 06:27:42 -0800

    Add new item: ch2, item16: How to handle missing dictionary keys: prefer get over in and keyError method

---
+++ Effective_Python/ch2_item16_prefer_get_over_in_and_keyError_handle_missing_dictionary_keys.py
@@ -0,0 +1,7 @@
+# The three fundamental operations for interacting with dictionaries:
+# 1. accessing
+# 2. assigning
+# 3. deleting keys and their associated values
+
+# The contents of dictionaries are dynamic, and thus it's entirely possbile - even likely -- when you try to access or
+# delete a key, it won't already be present.
