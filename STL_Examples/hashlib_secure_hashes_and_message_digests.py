1c9aa02270e332b8ce2e12702aafbfe0f4b89253
Lina <liuna.lina@gmail.com>
2023-01-26 05:30:27 -0800

    New Module: Hashlib: FIPS hash algorithms:SHA1, SHA224, SHA256, SHA384

---
+++ STL_Examples/hashlib_secure_hashes_and_message_digests.py
@@ -0,0 +1,3 @@
+# hashlib module implements a common interface to many different secure hash and message digest algorithms.
+
+# included FIPS secure hash algorithms SHA1, SHA224, SHA256, SHA384 and SHA512 as well as RSA's MD5 algorithm.
