bc79cba895275a21d20a600ac313d84f5e69486b
Lina <liuna.lina@gmail.com>
2023-01-27 06:26:53 -0800

    useful-example-secrets: generate an eight-character alphanumberic password

--- STL_Examples/secrets_generate_secure_random_numbers_for_managing_secrets.py
+++ STL_Examples/secrets_generate_secure_random_numbers_for_managing_secrets.py
@@ -1,7 +1,16 @@
 # The secrets module is used for generating cryptographically strong random numbers suitable for
 # managing data such as passwords, account authentication, security tokens, and related secrets.
 import secrets
+import string
 
 print(secrets.token_bytes(16))
 print(secrets.token_hex(16))
 print(secrets.token_urlsafe(16))
+
+def generate_8_alpha_numeric_password():
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(8))
+    return password
+
+print(generate_8_alpha_numeric_password())
+
