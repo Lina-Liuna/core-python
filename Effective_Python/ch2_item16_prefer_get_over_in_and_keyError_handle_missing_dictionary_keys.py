cef39e7840442312bdaf4360ccfb992f9c5a51a6
Lina <liuna.lina@gmail.com>
2023-01-27 05:35:00 -0800

    useful-example: restaurant votes: dicts get method to fetching a key that exists or returning a default value,

--- Effective_Python/ch2_item16_prefer_get_over_in_and_keyError_handle_missing_dictionary_keys.py
+++ Effective_Python/ch2_item16_prefer_get_over_in_and_keyError_handle_missing_dictionary_keys.py
@@ -3,5 +3,18 @@
 # 2. assigning
 # 3. deleting keys and their associated values
 
-# The contents of dictionaries are dynamic, and thus it's entirely possbile - even likely -- when you try to access or
+# The contents of dictionaries are dynamic, and thus it's entirely possible - even likely -- when you try to access or
 # delete a key, it won't already be present.
+
+
+restaurant_counters = {
+    'kio palace': 5,
+    'Imperial Delight': 4,
+    'Peking Delight' : 3,
+    'Szechuan Restaurant': 2,
+}
+key = 'lulu'
+count = restaurant_counters.get(key, 0)
+restaurant_counters[key] = count + 1
+print(restaurant_counters.items())
+
