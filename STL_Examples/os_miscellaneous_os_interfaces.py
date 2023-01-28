8b347453fc6ab0e6884a94703e1b1155e77e690b
Lina <liuna.lina@gmail.com>
2023-01-28 05:53:51 -0800

    useful-example: delete all files/subdirs in a directory by using os.walk/os.remove/os.rmdir/os.path.join

--- STL_Examples/os_miscellaneous_os_interfaces.py
+++ STL_Examples/os_miscellaneous_os_interfaces.py
@@ -12,4 +12,23 @@ def count_bytes_size_of_files_in_subdir(dir):
             dirs.remove('CSV')
 
 dir = '/Users/linaliu/code/data_analytics/'
+
 count_bytes_size_of_files_in_subdir(dir)
+
+def del_everything_in_dir(dir):
+    for root, dirs, files in os.walk(dir, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root,name))
+        for name in dirs:
+            os.rmdir(os.path.join(root,name))
+
+if not os.path.exists(dir+'temp'):
+    os.makedirs(dir+'temp')
+
+if not os.path.exists(dir+'temp/temp/'):
+    os.makedirs(dir+'temp/temp/')
+
+with open(dir+'temp/'+'temp.txt', 'w') as f:
+    f.write('hello temp test')
+
+del_everything_in_dir(dir + 'temp')
