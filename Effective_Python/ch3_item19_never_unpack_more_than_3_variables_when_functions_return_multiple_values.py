207be002505d9cc021f6bd7eefb8923492825287
Lina <liuna.lina@gmail.com>
2023-01-29 06:51:03 -0800

    Avoid-return-more-than-3-returns-example: use starred expression to get first and last item

--- Effective_Python/ch3_item19_never_unpack_more_than_3_variables_when_functions_return_multiple_values.py
+++ Effective_Python/ch3_item19_never_unpack_more_than_3_variables_when_functions_return_multiple_values.py
@@ -15,6 +15,14 @@
 # To avoid these problems, you should never use more than three variables when unpacking the multiple return values.
 # the return values can be three-tuple, two variables and one catch-all starred expression or namedtuple.
 
+def get_avg_ratio(numbers):
+    avarage = sum(numbers) / len(numbers)
+    scaled = [x / avarage for x in numbers]
+    scaled.sort(reverse=True)
+    return scaled
 
-
+number_list = [19, 40, 25, 56, 79, 92, 23]
+longest, *middle, shortest = get_avg_ratio(number_list)
+print(f'longest: {longest:>4.0%}')
+print(f'shortest:{shortest:>4.0%}')
 
