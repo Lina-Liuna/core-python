bd29ae0cbb7b6118c55326403219b5074dd5c7b3
Lina <liuna.lina@gmail.com>
2023-01-30 06:20:19 -0800

    Add new module: time: time access and conversions

---
+++ STL_Examples/time_time_access_and_conversions.py
@@ -0,0 +1,4 @@
+# The time module provides various time-related functions.
+# For related functionality, see also the datetime and calendar modules
+# Most of the functions defined in this module call platform C library functions with the same name.
+
