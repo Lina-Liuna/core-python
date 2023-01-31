d8e6ce825a759d9f917fd55d2079e80834443730
Lina <liuna.lina@gmail.com>
2023-01-31 06:27:47 -0800

    return False-equivalent-problem-solution: return a two-tuple

--- Effective_Python/ch3_item20_prefer_raising_exceptions_to_returning_none.py
+++ Effective_Python/ch3_item20_prefer_raising_exceptions_to_returning_none.py
@@ -2,3 +2,8 @@
 # if the return value is zero, it can cause issue when you evaluate the result in a condition like an if statement.
 # You might accidentally look for any False equivalent value to indicate error instead of only looking for none.
 # The misinterpretation of a False-equivalent return value is common mistake in Python code when None has special meaning
+
+# How to solve the misinterpretation of a False-equivalent return value?
+# The first way: split the return value into a two-tuple, the first part of the tuple
+# indicates that the operation was a success or failure, the second part is the actual result that was computed.
+
