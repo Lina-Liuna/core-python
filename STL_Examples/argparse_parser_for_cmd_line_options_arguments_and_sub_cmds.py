4bbd83ab1b9d4db3200b5631bc971f1d4ba87c37
Lina <liuna.lina@gmail.com>
2023-02-01 05:43:10 -0800

    Note-Example: use fromfile_prefix_chars=@ to keep the list of arguments in a file

--- STL_Examples/argparse_parser_for_cmd_line_options_arguments_and_sub_cmds.py
+++ STL_Examples/argparse_parser_for_cmd_line_options_arguments_and_sub_cmds.py
@@ -4,8 +4,30 @@
 
 import argparse
 
+# Step1: using the argparse to creating an ArgumentParser object
 parser = argparse.ArgumentParser(description='Process some integers.')
+
+# Step2: Fill the ArgumentParser with information about program arguments is done by making calls to the add_argument
 parser.add_argument('integers', metavar='N', type=int, nargs='+',help='an integer for the accumulator')
 parser.add_argument('--sum', dest='accumulate',action='store_const', const=sum, default=max, help='sum the integers')
+
+# Step3: call parse_args to inspect the command line, convertt each argument to the appropriate type and invoke the appropriate action.
 args = parser.parse_args()
 print(args.accumulate(args.integers))
+
+
+# Sometimes, when dealing with a particularly long argument list, it may make sense to keep the list of arguments in a file
+# rather than typing it out at the cmd line.
+
+# If the fromfile_prefix_chars= argument is given to the ArgumentParser constructor, then arguments that start with any
+# of the specified characters will be treated as files, and will be replacd by the arguments they contain.
+
+def parse_cmd_line_in_file(filename):
+    with open(filename, 'w') as fp:
+        fp.write('-f\nbar')
+    parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+    parser.add_argument('-f')
+    parser.parse_args(['-f', 'foo', '@args.txt'])
+
+dir = '/Users/linaliu/code/core-python/STL_Examples/'
+parse_cmd_line_in_file(dir+'args.txt')
