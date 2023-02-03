170cebc790eab72fa4924e650c51c76e8581d462
Lina <liuna.lina@gmail.com>
2023-02-03 06:45:35 -0800

    ch3,item21: Common-Solution: helper function as key argument to a list sort method

--- Effective_Python/ch3_item21_know_how_closures_interact_with_variable_scope.py
+++ Effective_Python/ch3_item21_know_how_closures_interact_with_variable_scope.py
@@ -3,3 +3,11 @@
 # why I want to solve this problem?
 # This pattern is useful when you're rendering a user interface and want important messages or exceptional event
 # to display before everything else.
+
+# How to solve prioritize one group of numbers to come first when sort a list?
+# A common way:
+# pass a helper function as the key argument to a list's sort method.
+# the helper's return value will be used as the value for sorting each item in the list.
+# the helper can check whether the given item is the important group and can vary the sorting value accordingly.
+
+
