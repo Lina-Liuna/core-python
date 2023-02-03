f08b9e903e076f6e1e254936f7d979b5c5a65e9b
Lina <liuna.lina@gmail.com>
2023-02-03 06:41:03 -0800

    Add ch3, item21: Problem: want to sort a list but prioritize one group of numbers to come first

---
+++ Effective_Python/ch3_item21_know_how_closures_interact_with_variable_scope.py
@@ -0,0 +1,5 @@
+# What problem to solve?
+# I want to sort a list of numbers but prioritize one group of numbers to come first.
+# why I want to solve this problem?
+# This pattern is useful when you're rendering a user interface and want important messages or exceptional event
+# to display before everything else.
