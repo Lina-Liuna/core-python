25fce54e430131e16ae3b85684962a6ff53de190
Lina <liuna.lina@gmail.com>
2023-02-04 06:28:24 -0800

    Sort-Priority: explain-the-reasons-why-it-worked

--- Effective_Python/ch3_item21_know_how_closures_interact_with_variable_scope.py
+++ Effective_Python/ch3_item21_know_how_closures_interact_with_variable_scope.py
@@ -23,4 +23,20 @@ group = {3,8,9,6}
 sort_priority(numbers, group)
 print(numbers)
 
+# WHy about code worked?
+# THere are three reasons this function operators as expected:
+
+# 1. python support closures--that is, functions that refer to variables from the scope in which they were defined.
+# This is why the helper unction is able to access the group argument for sort_priority
+
+# 2. Functions are first-class objects in python, which means you can refer them directly, assign them to variables,
+# pass them as arguments to other functions, compare them in expressions and if statements, and so on.
+# This is how the sort method can accept a closure function as the key argument
+
+# 3. Python has specific rules for comparing sequences(including tuples). it first compare items at index zero;
+# then if those are equal, it compares items at index one, if they are still equal, it compares items at index two.
+# This is why the return value from the helper closure causes the sort order to have two distinct groups.
+
+
+
 
