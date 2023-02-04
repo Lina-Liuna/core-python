5104c592b2962b5987c1d36051372eb106f2cd75
Lina <liuna.lina@gmail.com>
2023-02-04 06:15:50 -0800

    Sort-Priority: example: helper-method-in-sort

--- Effective_Python/ch3_item21_know_how_closures_interact_with_variable_scope.py
+++ Effective_Python/ch3_item21_know_how_closures_interact_with_variable_scope.py
@@ -10,4 +10,17 @@
 # the helper's return value will be used as the value for sorting each item in the list.
 # the helper can check whether the given item is the important group and can vary the sorting value accordingly.
 
+def sort_priority(values, group):
+    def helper(x):
+        if x in group:
+            return (0, x)
+        return (1, x)
+    values.sort(key=helper)
+
+
+numbers = [5,3,6,8,9,0,2]
+group = {3,8,9,6}
+sort_priority(numbers, group)
+print(numbers)
+
 
