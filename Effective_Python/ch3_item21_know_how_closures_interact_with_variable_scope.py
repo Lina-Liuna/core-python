a15d4d82092048217a272afe35cba506a1841545
Lina <liuna.lina@gmail.com>
2023-02-04 06:39:52 -0800

    Sort-Priority-flip-flag: defect-Solution

--- Effective_Python/ch3_item21_know_how_closures_interact_with_variable_scope.py
+++ Effective_Python/ch3_item21_know_how_closures_interact_with_variable_scope.py
@@ -37,6 +37,29 @@ print(numbers)
 # then if those are equal, it compares items at index one, if they are still equal, it compares items at index two.
 # This is why the return value from the helper closure causes the sort order to have two distinct groups.
 
+# Advanced Problem:
+# It will be nice if sort_priority function returned whether higher-priority items were seen at all so the user interface
+# code can act accordingly.
+
+# Solution:
+# flip a flag when high-priority items are seen.
+# Then the function can return the flag value after it's been modified by the closure.
+
+def sort_priority2(numbers, group):
+    found = False
+    def helper(x):
+        if x in group:
+            found = True
+            return (0, x)
+        return (1, x)
+    numbers.sort(key=helper)
+    return found
+
+numbers = [5,3,6,8,9,0,2]
+group = {3,8,9,6}
+print(sort_priority2(numbers, group))
+print(numbers)
+
 
 
 
