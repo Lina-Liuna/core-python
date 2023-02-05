39d743be1cf03df3faf20f79b813dad8a3f0fb25
Lina <liuna.lina@gmail.com>
2023-02-05 05:39:37 -0800

    Add-ch3-item22: use positional-argument, alias: varargs and *args

---
+++ Effective_Python/ch3_item22_reduce_visual_noise_with_variable_positional_arguments.py
@@ -0,0 +1,2 @@
+# Accepting a variable number of positional arguments can make a function call clearer and reduce visual noise.
+# These positional arguments are often called varargs for short, or star args. *args.
