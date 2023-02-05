30e23e9d147ebd5a799c87bb93b49b6e2ea52f4e
Lina <liuna.lina@gmail.com>
2023-02-05 05:57:20 -0800

    varargs-*args-star-args-example: can ignore the empty input as *args

--- Effective_Python/ch3_item22_reduce_visual_noise_with_variable_positional_arguments.py
+++ Effective_Python/ch3_item22_reduce_visual_noise_with_variable_positional_arguments.py
@@ -13,3 +13,19 @@ def log(message, values):
 
 log('Today is Sunday, time is', [5, 43])
 log('Raining outside currently', [])
+
+# problem of above solution:
+# pass an empty list when have no values to log is cumbersome and noisy.
+# It'd be better to leave out the second argument entirely.
+# I can do this in python by prefixing the last positional parameter name with *.
+# The first parameter for the log message is required, whereas any number of subsequent positional arguments are optional
+
+def log(message, *values):
+    if not values:
+        print(message)
+    else:
+        value_str = ':'.join(str(x) for x in values)
+        print(f'{message}: {value_str}')
+
+log('Dairy log: Today is Sunday, time is', [5, 43])
+log('Dairy log: Raining outside currently')
