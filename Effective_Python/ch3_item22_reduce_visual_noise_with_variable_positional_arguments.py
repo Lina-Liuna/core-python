9beb98648b47432fd839d593e74177ad34f9adcf
Lina <liuna.lina@gmail.com>
2023-02-05 05:46:46 -0800

    log-example-as-list-input-argument: defect: empty list is cumbersome/noisy

--- Effective_Python/ch3_item22_reduce_visual_noise_with_variable_positional_arguments.py
+++ Effective_Python/ch3_item22_reduce_visual_noise_with_variable_positional_arguments.py
@@ -1,2 +1,15 @@
 # Accepting a variable number of positional arguments can make a function call clearer and reduce visual noise.
 # These positional arguments are often called varargs for short, or star args. *args.
+
+# example: log some debugging information. with a fixed number of arguments, I would need a function that takes a msg
+# and a list of values.
+
+def log(message, values):
+    if not values:
+        print(message)
+    else:
+        values_str = ':'.join(str(x) for x in values)
+        print(f'{message}: {values_str}')
+
+log('Today is Sunday, time is', [5, 43])
+log('Raining outside currently', [])
