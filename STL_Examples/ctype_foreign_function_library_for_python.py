1914ed19a8cc45e8e1583a09fa3ffaaf7126f23e
Lina <liuna.lina@gmail.com>
2023-02-06 06:36:55 -0800

    cypte-module-example: call-C-Compatible-data-types-in-python

--- STL_Examples/ctype_foreign_function_library_for_python.py
+++ STL_Examples/ctype_foreign_function_library_for_python.py
@@ -4,3 +4,9 @@
 
 import ctypes
 print(ctypes.cdll.LoadLibrary('/usr/local/lib/python3.9/site-packages/cv2/cv2.abi3.so'))
+print(ctypes.c_int())
+print(ctypes.c_wchar_p("Good Morning, today is Feb 6, 2023, 06:33"))
+
+s = "Good Morning, today is Feb 6, 2023, 06:33"
+c_s = ctypes.c_wchar_p(s)
+print(c_s.value)
