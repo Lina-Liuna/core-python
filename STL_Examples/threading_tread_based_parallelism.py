a102876fe4dd445db8ef849b6b0b80302551a955
Lina <liuna.lina@gmail.com>
2023-02-07 05:48:32 -0800

    useful-timer-example: wait-15-seconds-before-eatting-random-choice-bread

--- STL_Examples/threading_tread_based_parallelism.py
+++ STL_Examples/threading_tread_based_parallelism.py
@@ -11,9 +11,45 @@
 # multiple operating system threads.
 
 import threading
+import random
 
 # thread-local data
 # thread-local data is data whose values are thread specific.
 
 mydata = threading.local
 mydata = 4
+
+# The run() method invokes the callable object passed to the objects's constructor as the target argument.
+t = threading.Thread(target=print, args=[1])
+t.run()
+
+t = threading.Thread(target=print, args=(1,))
+t.run()
+
+# Timer Objects
+# This class represents an action that should be run only after a certain amount of time has passed.
+
+def eat_breads():
+    types_of_breads = list()
+    types_of_breads = [
+        'white bread',
+        'multigrain bread',
+        'rye',
+        'pretzel',
+        'hot dog bread',
+        'swiss roll',
+        'croissant',
+        'hamburger',
+        'baguette',
+        'donut',
+        'bagels',
+    ]
+
+    random_choice = random.choices(types_of_breads, k=1)
+
+    print(f'Today\'s morning bread is: {random_choice}')
+
+t = threading.Timer(15, eat_breads)
+t.start()
+
+
