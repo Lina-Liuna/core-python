67a87fdfd9754ad05bdc3bda9b0abb380013c06c
Lina <liuna.lina@gmail.com>
2023-02-07 05:27:18 -0800

    Add-thread-module: Compare: concurrent-queue-asyncio 3 modules

---
+++ STL_Examples/threading_tread_based_parallelism.py
@@ -0,0 +1,13 @@
+# treading module constructs higher-level threading interfaces on top of the lower level _thread module.
+
+# see also:
+# 1. concurrent.futures.ThreadPoolExecutor offers a higher level interface to push tasks
+# to a background thread without blocking execution of the calling thread, while still being able to
+# retrieve their results when needed.
+
+# 2. queue provides a thread-safe interface for exchanging data between running threads
+
+# asyncio offers an alternative approach to achieving task level concurrency without requiring the use of
+# multiple operating system threads.
+
+#
