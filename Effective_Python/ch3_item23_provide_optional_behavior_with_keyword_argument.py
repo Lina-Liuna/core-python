aff8307d1c3843f958310f46b16d7b358664b604
Lina <liuna.lina@gmail.com>
2023-02-07 06:27:45 -0800

    **operator-as-input:pass **dictionary in function call

--- Effective_Python/ch3_item23_provide_optional_behavior_with_keyword_argument.py
+++ Effective_Python/ch3_item23_provide_optional_behavior_with_keyword_argument.py
@@ -1,2 +1,23 @@
 # you can assign keyword arguments withing the parentheses of a function call.
 # you can mix and match keyword and positional arguments.
+import collections
+
+acronym_words = {
+    'FYI': 'For your information',
+    'BTW': 'By the way',
+    'GR8': 'Great',
+    'IDK': 'I don\'t know',
+    'IDC': 'I don\'t care',
+    'JK' : 'Just kidding',
+
+}
+
+def words(word, meaning):
+    print(f'{word}:{meaning}')
+
+#words(**acronym_words)     #error, dont find word and meaning
+acronym_words = {
+    'word': 'FYI',
+    'meaning': 'For your information',
+}
+words(**acronym_words)
