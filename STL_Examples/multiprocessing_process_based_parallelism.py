acf29456a0eecc7277cfdaa61c2ce1f48fbd175c
Lina <liuna.lina@gmail.com>
2023-02-08 05:17:22 -0800

    Add-multiprocessing-package: multiprocessing-usage: spawning processes, offers concurrency, use the global interpreter lock

---
+++ STL_Examples/multiprocessing_process_based_parallelism.py
@@ -0,0 +1,6 @@
+# multiprocessing is a package that supports spawning processes using an API similar to the threading module.
+# the multiprocessing package offers both local and remote concurrency.
+# effectively side-stepping the global interpreter lock by using subprocesses instead of threads.
+# the multiprocessing module allows the programmer to fully leverage multiple processors on a given machine.
+
+
