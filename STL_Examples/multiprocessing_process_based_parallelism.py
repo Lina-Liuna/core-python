61bda7078885c0ff65e8a06114f06eb2dc4e65bc
Lina <liuna.lina@gmail.com>
2023-02-08 05:24:09 -0800

    multiprocessing-pool-example: parallelizing the execution of a function across multiple input values

--- STL_Examples/multiprocessing_process_based_parallelism.py
+++ STL_Examples/multiprocessing_process_based_parallelism.py
@@ -3,4 +3,15 @@
 # effectively side-stepping the global interpreter lock by using subprocesses instead of threads.
 # the multiprocessing module allows the programmer to fully leverage multiple processors on a given machine.
 
+# pool object offers a convenient means of parallelizing the execution of a function across multiple input values.
+# pool object distributing the input data across processes.
 
+# the basic example of data parallelism using pool
+
+import multiprocessing
+
+def f(x):
+    return x * x
+if __name__ == '__main__':         # must contain this line!!!!
+    with multiprocessing.Pool(5) as p:
+        print(p.map(f, [1,2,3,4,5,6,7,8,9]))
