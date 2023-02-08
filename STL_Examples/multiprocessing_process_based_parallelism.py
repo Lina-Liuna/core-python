e64698008580de4694380f2886939bed4d14b9bf
Lina <liuna.lina@gmail.com>
2023-02-08 05:41:16 -0800

    use multiprocessing.set_start_method('spawn'/'fork'/'forkserver') to choose one of the 3 ways to start a process

--- STL_Examples/multiprocessing_process_based_parallelism.py
+++ STL_Examples/multiprocessing_process_based_parallelism.py
@@ -8,12 +8,16 @@
 
 # the basic example of data parallelism using pool
 
-import multiprocessing
+import multiprocessing as mp
 
 def f(x):
     return x * x
-if __name__ == '__main__':         # must contain this line!!!!
-    with multiprocessing.Pool(5) as p:
+
+def foo(q):
+    q.put('hello')
+
+def parallelism_with_pool():
+    with mp.Pool(5) as p:
         print(p.map(f, [1,2,3,4,5,6,7,8,9]))
 
 # multiprocessing supports 3 ways to start a process.
@@ -23,4 +27,18 @@ if __name__ == '__main__':         # must contain this line!!!!
 
 # 1. spawn: inherit limited resources, slow compared to using fork or forkserver
 # 2. fork: use os.fork() is identical to the parent process, how to safely forking is problematic
-# 3. forkserver: whenever needed, the parent process connects to the server and request that it fork a new process.f
+# 3. forkserver: whenever needed, the parent process connects to the server and request that it fork a new process.
+def use_set_start_method():
+    mp.set_start_method('spawn')
+    q = mp.Queue()
+    p = mp.Process(target=foo, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
+
+if __name__ == '__main__':         # must contain this line!!!!
+    #parallelism_with_pool()
+    use_set_start_method()
+
+
+
