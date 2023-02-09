1ec625a199552ebc50870a4e70bfb74d31b9da54
Lina <liuna.lina@gmail.com>
2023-02-09 05:36:27 -0800

    SharedMemory-instance example: Low-level-use-of_sharedmemory

--- STL_Examples/multiprocessing_shared_memory_for_direct_access_across_processes.py
+++ STL_Examples/multiprocessing_shared_memory_for_direct_access_across_processes.py
@@ -12,3 +12,30 @@
 # containing that data.
 
 # Sharing data directly via memory can provide significant performance benefits compared to sharing data via disk.
+import multiprocessing as mp
+from multiprocessing.shared_memory import SharedMemory
+
+# low-level use of SharedMemory instances:
+
+def low_level_use_of_SharedMemory():
+    shm_a = SharedMemory(create=True, size=10)
+    print(type(shm_a.buf))
+    buffer = shm_a.buf
+    print(len(buffer))
+    buffer[:5] = bytearray([11,22,33,44,55])
+    buffer[5] = 100
+    shm_b = SharedMemory(shm_a.name)
+
+    import array
+    array.array('b', shm_b.buf[:5])
+
+    print(shm_b)
+    print(shm_b.buf)
+    print(bytes(shm_b.buf))
+    print(bytes(shm_b.buf[:10]))
+
+    shm_b.buf[:5] = b'howdy'
+    print(bytes(shm_b.buf[:10]))
+    print(bytes(shm_a.buf[:10]))
+
+low_level_use_of_SharedMemory()
