70ce86963ae96d40a258d907152f7ea14fa798fb
Lina <liuna.lina@gmail.com>
2023-02-13 13:38:03 -0800

    ProcessPoolExecutor Example

--- STL_Examples/concurrent_futures_lunching_parallel_tasks.py
+++ STL_Examples/concurrent_futures_lunching_parallel_tasks.py
@@ -42,3 +42,36 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
             print('%r generated an exception: %s' % (url, exc))
         else:
             print('%r page is %d bytes' % (url, len(data)))
+
+import concurrent.futures
+import math
+
+PRIMES = [
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099,
+    1099726899285419]
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+
+if __name__ == '__main__':
+    main()
