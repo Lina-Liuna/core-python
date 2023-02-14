4953005e5e1704ebc30ecd8697ff38ee920feb9c
Lina <liuna.lina@gmail.com>
2023-02-14 05:30:40 -0800

    json-decode-example: default argument value is important when arguments are mutable

--- Effective_Python/ch3_item24_use_none_and_docstrings_to_specify_dynamic_default_arguments.py
+++ Effective_Python/ch3_item24_use_none_and_docstrings_to_specify_dynamic_default_arguments.py
@@ -17,13 +17,41 @@ log('lina liu login again')        # time in log message not changed!!!
 
 # A default argument value is evaluated only once per module load, which usually happens when a program starts up.
 # After the module containing this code is loaded, the datetime.now() default argument will never be evaluated again.
-
 def log(message, when=None):
     if when is None:
         when = datetime.datetime.now()
-        print(f'{when}: {message}')
+    print(f'{when}: {message}')
 
 log('lina liu diary log info')
 time.sleep(0.3)
 log('lina liu login again')
+
+# Use None for default argument values is especially important when the arguments are mutable.
+
+# Example : I want to load a value encoded as JSON data, if decoding the data fails, I want an empty dictionary
+# to be returned by default.
+
+import json
+
+def decode(data, default={}):
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
+
+
+json_data = decode('{"bad data": "3", "huff": "5"}')
+json_data['huff'] = 3
+
+json_bar = decode('{bad again}')
+json_bar['beep'] = 1
+print(f'json_data={json_data}, json_bar={json_bar}')
+
+foo = decode('bad data')
+foo['stuff'] = 5
+bar = decode('also bad')
+bar['meep'] = 1
+print(f'foo={foo}, bar={bar}')
+
+
 
