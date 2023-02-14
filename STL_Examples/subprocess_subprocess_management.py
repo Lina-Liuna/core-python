19875a88be092ac6f0e2777a185908e71100e7ee
Lina <liuna.lina@gmail.com>
2023-02-14 06:03:56 -0800

    Useful: Run-ls-l-cmd-in-script-by-using-subprocess-run

---
+++ STL_Examples/subprocess_subprocess_management.py
@@ -0,0 +1,4 @@
+# The subprocess module allows you to spawn new processes,
+# connect to their input/output/error pipes, and obtain their return codes.
+import subprocess
+subprocess.run(["ls", "-l"])
