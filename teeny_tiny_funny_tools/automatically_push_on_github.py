38d9b994f857f41dbcc74e07dd4b58dc57081f36
Lina <liuna.lina@gmail.com>
2023-02-14 06:43:40 -0800

    funny-tool: automatically submit and push on github

---
+++ teeny_tiny_funny_tools/automatically_push_on_github.py
@@ -0,0 +1,36 @@
+import subprocess
+import os
+import datetime
+import schedule
+import time
+
+
+
+file = os.path.basename(__file__)
+
+
+
+def log_msg(file=os.path.basename(__file__), message="auto-sumbit-message:", when=None):
+    if when is None:
+        when = datetime.datetime.now()
+    with open(file, 'w') as f:
+        f.write(f'{when}: {message}')
+    print(f'{when}: {message}')
+
+def git_submit(file=os.path.basename(__file__)):
+    log_msg(file)
+    subprocess.run(['git', 'add', file])
+    subprocess.run(['git', 'commit', '-m', 'automatically commit a github by using subprocess run'])
+    subprocess.run(['git', 'push'])
+
+def auto_submit(file=os.path.basename(__file__)):
+    #schedule.every().day.at("01:00").do(git_submit, 'It is 01:00')o
+    schedule.every(1).minutes.do(git_submit, 'It is 01:00')
+
+    while True:
+        # Checks whether a scheduled task
+        # is pending to run or not
+        schedule.run_pending()
+        time.sleep(1)
+
+auto_submit()
