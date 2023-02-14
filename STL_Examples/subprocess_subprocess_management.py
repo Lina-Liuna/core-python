c39964f8fef2fc6baf3271611bceb2bd5b45262a
Lina <liuna.lina@gmail.com>
2023-02-14 06:15:33 -0800

    commit a github by using subprocess run

--- STL_Examples/subprocess_subprocess_management.py
+++ STL_Examples/subprocess_subprocess_management.py
@@ -1,4 +1,15 @@
 # The subprocess module allows you to spawn new processes,
 # connect to their input/output/error pipes, and obtain their return codes.
 import subprocess
+import os
+
 subprocess.run(["ls", "-l"])
+
+subprocess.run(["ls", "-l", "/dev/null"], capture_output=True)
+subprocess.run(["ls", "-l", "/dev/null"])
+subprocess.Popen(["/usr/bin/git", "commit", "-m", "Fixes a bug."])
+subprocess.Popen(["/usr/bin/git","push"])
+
+subprocess.run(['git', 'add',os.path.basename(__file__)])
+subprocess.run(['git', 'commit', '-m', 'commit a github by using subprocess run'])
+subprocess.run(['git', 'push'])
