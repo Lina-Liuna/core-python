ea4db51753e81c70fbfd146f72b3e835285e70ed
Lina <liuna.lina@gmail.com>
2023-02-15 06:07:09 -0800

    Add-ch3-item25: Why-keyword-arguments-is-important

---
+++ Effective_Python/ch3_item25_enforce_clarity_with_keyword-only_and_positional-only_arguments.py
@@ -0,0 +1,8 @@
+# Passing arguments by keyword is powerful feature of python functions.
+
+# The flexibility of keyword arguments enables you to write functions for many use cases.
+
+# too many arguments may cause position-confusing, and this is exception-ignoring behavior, cause bugs
+# that are hard to track down.
+
+#
