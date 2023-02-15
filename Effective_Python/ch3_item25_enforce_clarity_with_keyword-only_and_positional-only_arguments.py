8a3dddd4d5f085af6398d5d0dabc9644da65c225
Lina <liuna.lina@gmail.com>
2023-02-15 06:12:06 -0800

    ch3-item25: defining-keyword-only-arguments-instead-of-by-position?

--- Effective_Python/ch3_item25_enforce_clarity_with_keyword-only_and_positional-only_arguments.py
+++ Effective_Python/ch3_item25_enforce_clarity_with_keyword-only_and_positional-only_arguments.py
@@ -5,4 +5,9 @@
 # too many arguments may cause position-confusing, and this is exception-ignoring behavior, cause bugs
 # that are hard to track down.
 
-#
+# One way to improve the readability of this code is to use keyword arguments.
+
+# But the problem is here: keyword arguments are optional behavior, there's nothing forcing callers to use
+# keyword arguments for clarity.
+
+# defining keyword-only arguments, the arguments can only be supplied by keword, never by position.
