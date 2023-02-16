024d2566cbef985a67e90b54a8874dd1e12aca92
Lina <liuna.lina@gmail.com>
2023-02-16 07:30:14 -0800

    USEFUL-EXAMPLE: print argument and return value of func call for debugging in recursive functions

--- Effective_Python/ch3_item26_define_function_decorators_with_functools_wraps.py
+++ Effective_Python/ch3_item26_define_function_decorators_with_functools_wraps.py
@@ -6,3 +6,29 @@
 
 # decorators can be useful for enforcing sematics, debugging, registering functions, and more.
 
+# Example:
+# I want to print the arguments and return value of a function call
+# This example is helpful when debugging the stack of nested function calls from a recursive function.
+def trace(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print(f'{func.__name__}({args!r}, {kwargs!r}) '
+              f'-> {result!r}')
+        return result
+    return wrapper
+
+#I can apply this decorator to a function by using the @ symbol:
+@trace
+def fibonacci(n):
+    """Return the n-th Fibonacci number"""
+    if n in (0, 1):
+        return n
+    return (fibonacci(n - 2) + fibonacci(n - 1))
+
+fibonacci = trace(fibonacci)
+
+fibonacci(4)
+
+
+
+
