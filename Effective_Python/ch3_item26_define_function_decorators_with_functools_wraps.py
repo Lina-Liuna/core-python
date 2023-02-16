aafccf32824e0dc2e93a4727fda943308f87706b
Lina <liuna.lina@gmail.com>
2023-02-16 07:25:44 -0800

    NOTE-TO-REMEMBER: decorators applied to functions can access and modify functions

---
+++ Effective_Python/ch3_item26_define_function_decorators_with_functools_wraps.py
@@ -0,0 +1,8 @@
+# decorators that can be applied to functions
+
+# A decorator has the ability to run additional code before and after each call to a function wraps.
+
+# decorators can access and modify input arguments, return values, and raised exceptions.
+
+# decorators can be useful for enforcing sematics, debugging, registering functions, and more.
+
