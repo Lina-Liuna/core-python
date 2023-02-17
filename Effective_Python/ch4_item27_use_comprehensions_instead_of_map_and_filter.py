d60554af4754ac4710cf700f99597c60a35c5f70
Lina <liuna.lina@gmail.com>
2023-02-17 06:26:13 -0800

    comprehension-generator-advantage: geneartor can improve proformance, reduce memory usage, and increase readability

---
+++ Effective_Python/ch4_item27_use_comprehensions_instead_of_map_and_filter.py
@@ -0,0 +1,7 @@
+# Comprehensions is extended to functions with generators
+
+# the generator enable a stream of values to be incrementally returned by a function.
+
+# The result of a call to a generator function can be used anywhere an iterator is appropriate.
+
+# Generators can improve performance, reduce memory usage, and increase readability.
