24678d6cca6c44f16f9a6eee71d836fdf330d8f0
Lina <liuna.lina@gmail.com>
2023-02-17 06:31:22 -0800

    what-is-list-comprehension-exmaple: duplicate every item in letter list 3 times

--- Effective_Python/ch4_item27_use_comprehensions_instead_of_map_and_filter.py
+++ Effective_Python/ch4_item27_use_comprehensions_instead_of_map_and_filter.py
@@ -5,3 +5,13 @@
 # The result of a call to a generator function can be used anywhere an iterator is appropriate.
 
 # Generators can improve performance, reduce memory usage, and increase readability.
+
+
+# what is list comprehensions?
+# the compact syntax for deriving a new list from another sequence or iterable.
+
+a = ['a', 'b', 'c', 'd', 'e']
+
+# suare_a is a list comprehension
+square_a = [x*3 for x in a]
+print(square_a)
