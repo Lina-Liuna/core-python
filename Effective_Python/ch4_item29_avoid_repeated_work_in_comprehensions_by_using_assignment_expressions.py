230312c57944fb46cb50e18d9e8b7bd29f9b66dc
Lina <liuna.lina@gmail.com>
2023-02-19 06:44:49 -0800

    useful-example: writing a program to manage orders fro a shoes store

---
+++ Effective_Python/ch4_item29_avoid_repeated_work_in_comprehensions_by_using_assignment_expressions.py
@@ -0,0 +1,13 @@
+# writing a program to manage orders for a shoes store.
+# as new orders come in from customers, I need to be able to tell them whether I can fulfill their orders.
+# I need to verify that a request is sufficiently in stock and above the minimum threshold for shopping.
+
+shoes_color_stock = {
+    'write' : 100,
+    'floral': 10,
+    'black' : 60,
+    'pink' :20,
+    'mix':5
+}
+
+order = ['floral', 'black', 'pink']
