ac460d074d4e529d9c90be1d6e52f71514eb3bf5
Lina <liuna.lina@gmail.com>
2023-02-20 20:34:29 -0800

    json-example: use json.dumps with sort_keys/separators/indent to convert python dict into a json string

--- STL_Examples/json_json_encoder_and_decoder.py
+++ STL_Examples/json_json_encoder_and_decoder.py
@@ -13,3 +13,9 @@ import json
 # why print [] square brackets instead of () Parentheses?
 print(json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]))
 print(json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True))
+
+print(json.dumps([1,2,3,{'4':5, '6': 7}]))
+print(json.dumps([1,2,3,{'4':5, '6': 7}], separators=(',',':')))
+print(json.dumps([1,2,3,{'4':5, '6': 7}], separators=('','')))
+print(json.dumps([1,2,3,{'4':5, '6': 7}], separators=('?','*')))
+print(json.dumps([1,2,3,{'4':5, '6': 7}], separators=('--------','-----')))
