98725ed57d2b962016c19c5cca7cd0dfdcf6021d
Lina <liuna.lina@gmail.com>
2023-02-20 19:59:56 -0800

    JSON(javascript-object-notation) module

---
+++ STL_Examples/json_json_encoder_and_decoder.py
@@ -0,0 +1,8 @@
+# What is JSON?
+# JavaScript Object Notation, is a lightweight data interchange format inspired by javascript object literal syntax.
+
+# Warning: Be cautious when parsing JSON data from untrusted sources.
+# A malicious JSON string may cause the decoder to consume considerable CPU and memory resources.
+# Limiting the size of data to be parsed is recommended.
+
+# json exposes an API familiar to users of the standard library marshal and pickle modules.
