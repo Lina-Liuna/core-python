41007a2403f98b292e7068e04df0a95951e4ff72
Lina <liuna.lina@gmail.com>
2023-02-22 05:45:22 -0800

    generator-example: stream input from a file one line at a time and yields outputs one words at a time

--- Effective_Python/ch4_item30_consider_generators_instead_of_returning_lists.py
+++ Effective_Python/ch4_item30_consider_generators_instead_of_returning_lists.py
@@ -1,9 +1,14 @@
 # we often return list of items
+# print(len('# we often return list of items'))
 # what's the side effect of return list of items?
 # There is one line for creating the result list and another for returning it.
+# all results to be stored in the list before being returned, for huge inputs, this can cause a program
+# to run out of memory and crash.
+
 
 # A better way to write this function is by using a generator.
 # Generators are produced by functions that use yield expressions.
+# return a generator can easily be adapted to take inputs of arbitrary length due to its bounded memory requirements.
 
 # Example: I want to find the index of every word in a string.
 def index_words_iter(text):
@@ -15,3 +20,25 @@ def index_words_iter(text):
 
 car_words = "steering wheel, clutch, brake, gas pedal, gear level, speedometer, rearview mirror, dashboard"
 print(list(index_words_iter(car_words)))
+
+# Example: I define a generator that streams input from a file one line at a time and yields outputs one word at a time
+def index_file(handle):
+    offset = 0
+    for line in handle:
+        if line:
+            yield offset
+        for letter in line:
+            offset += 1
+            if offset == ' ':
+                yield offset
+
+import os, itertools
+
+with open(os.path.basename(__file__), 'r') as f:
+    it = index_file(f)
+    res = itertools.islice(it, 0, 5)
+    print(list(res))
+
+
+
+
