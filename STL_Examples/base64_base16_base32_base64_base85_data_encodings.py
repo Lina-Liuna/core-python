40ec27b7ba30deda11936e90abf26f66ca32711e
Lina <liuna.lina@gmail.com>
2023-02-23 13:03:36 -0800

    base64.b64encode example

---
+++ STL_Examples/base64_base16_base32_base64_base85_data_encodings.py
@@ -0,0 +1,9 @@
+# This module provides functions for encoding binary data to printable ASCII characters and decoding such encoding back to binary data
+
+import base64
+
+encoded = base64.b64encode(b'data to be encoded')
+print(encoded)
+
+data = base64.b64decode(encoded)
+print(data)
