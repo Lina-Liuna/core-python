dbbfa49c717048320d6c8da3162fcaf2c430eab1
Lina <liuna.lina@gmail.com>
2023-02-24 05:42:41 -0800

    example: analyze tourism number for the U.S. state

---
+++ Effective_Python/ch4_item31_be_defensive_when_iterating_over_arguments.py
@@ -0,0 +1,7 @@
+# Example: I want to analyze tourism numbers for the U.S. state of Texas,
+# the data set is the number of visitors to each city
+# I'd like to figure out what percentage of overall tourism each city receives.
+
+# I need a normalization function that sums the inputs to determine the total number of tourists per year
+# and then divides each city's individual visitor count by the total to find that city's contribution to the whole.
+
