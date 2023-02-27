0c36b7fcb2ff99939277dcf51379922c0d590dde
Lina <liuna.lina@gmail.com>
2023-02-27 06:29:51 -0800

    multiple-generator-example: excellent graphic program to animate the movement of images onscreen

--- Effective_Python/ch4_item33_compose_multiple_generators_with_yield_from.py
+++ Effective_Python/ch4_item33_compose_multiple_generators_with_yield_from.py
@@ -5,3 +5,29 @@
 # and then continue moving at a slower pace.
 
 # I define two generators that yield the expected onscreen deltas for each part of this animation.
+
+def move(period, speed):
+    for _ in range(period):
+        yield speed
+
+def pause(delay):
+    for _ in range(delay):
+        yield 0
+
+def animate():
+    for delta in move(4, 5.0):
+        yield delta
+    for delta in pause(3):
+        yield delta
+    for delta in move(2, 3.0):
+        yield delta
+
+def render(delta):
+    print(f'Delta:{delta:.1f}')
+    # Move the images onscreen
+
+def run(func):
+    for delta in func():
+        render(delta)
+
+run(animate)
