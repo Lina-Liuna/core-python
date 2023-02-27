887fa2e5ea7d059f976ddcfab62b90ec664368d2
Lina <liuna.lina@gmail.com>
2023-02-27 07:08:45 -0800

    excellent-micro-benchmark-example: compare time speed between yield from expression and normal nested yield

--- Effective_Python/ch4_item33_compose_multiple_generators_with_yield_from.py
+++ Effective_Python/ch4_item33_compose_multiple_generators_with_yield_from.py
@@ -46,3 +46,36 @@ def animate_composed():
     yield from move(2, 3.0)
 
 run(animate_composed)
+
+
+# yield from expression essentialy causes the python interpreter to handle the nested for loop and yield expression
+# boilerplate for you, which results in better performance.
+
+# Excellent example: verify the speedup by using the timeit built-in module to run a micro-benchmark:
+
+import timeit
+
+def child():
+    for i in range(1_000_000):
+        yield i
+
+def slow():
+    for i in child():
+        yield i
+
+def fast():
+    yield from child()
+
+baseline = timeit.timeit(stmt='for _ in slow(): pass',
+                         globals=globals(),
+                         number=50)
+print(f'Manual nesting {baseline:.2f}s')
+
+comparison = timeit.timeit(stmt='for _ in fast(): pass',
+                           globals=globals(),
+                           number=50)
+print(f'Composed nesting {comparison:.2f}s')
+
+reduction = -(comparison - baseline) / baseline
+print(f'{reduction:.1%} less time')
+
