2ce627122be67cf2e59563d46277c9236cefaee7
Lina <liuna.lina@gmail.com>
2023-02-28 06:24:06 -0800

    stream-data-in-out-generator-example: a program to transmit signals

--- Effective_Python/ch4_item34_avoid_injecting_data_into_generators_with_send.py
+++ Effective_Python/ch4_item34_avoid_injecting_data_into_generators_with_send.py
@@ -1,7 +1,31 @@
 # yield expressions provide generator functions with a simple way to produce an iterable series of output values.
-# this channel appears to be unidirectional: there is no immediately obvious way to simultneously stream data in and
+# this channel appears to be unidirectional: there is no immediately obvious way to simultaneously stream data in and
 # out of a generator as it runs.
 
 # Example: I'm writing a program to transmit signals using a software-defined radio.
 # I use a function to generate an approximation of a sine wave with a given number of points.
+import math
 
+
+def wave(amplitude, steps):
+    step_size = 2 * math.pi / steps
+
+    for step in range(steps):
+        radians = step * step_size
+        fraction = math.sin(radians)
+        output = amplitude * fraction
+        yield output
+
+
+def transmit(output):
+    if output is None:
+        print(f'Output is None')
+    else:
+        print(f'Output: {output:>5.1f}')
+
+
+def run(it):
+    for output in it:
+        transmit(output)
+
+run(wave(3.0, 8))
