67bc9282f17ac8ae00375bb751639096a4916358
Lina <liuna.lina@gmail.com>
2023-02-28 06:18:07 -0800

    way-to-stream-data-in-out-a-generator: example description

---
+++ Effective_Python/ch4_item34_avoid_injecting_data_into_generators_with_send.py
@@ -0,0 +1,7 @@
+# yield expressions provide generator functions with a simple way to produce an iterable series of output values.
+# this channel appears to be unidirectional: there is no immediately obvious way to simultneously stream data in and
+# out of a generator as it runs.
+
+# Example: I'm writing a program to transmit signals using a software-defined radio.
+# I use a function to generate an approximation of a sine wave with a given number of points.
+
