fc7af285a9baa0cc0d3452026c9514ef3b266f1b
Lina <liuna.lina@gmail.com>
2023-03-02 05:56:44 -0800

    generator-advanced-throw-method: how throw method works? example

--- Effective_Python/ch4_item35_avoid_causing_state_transitions_in_generators_with_throw.py
+++ Effective_Python/ch4_item35_avoid_causing_state_transitions_in_generators_with_throw.py
@@ -2,3 +2,28 @@
 # 1. yield from expression
 # 2. send method
 # 3. throw method for re-raising exception instances within generator functions
+
+# How throw method works?
+# The way throw works is simple:
+# When the method is called, the next occurrence of a yield expression re-raises the provided
+# Exception instance after its output is received instead of continuing normally.
+
+# Example:
+
+class MyError(Exception):
+    pass
+
+def yield_iter():
+    for i in range(1, 4):
+        yield i
+
+def my_generator():
+    yield from yield_iter()
+
+it = my_generator()
+print(next(it))
+print(next(it))
+print(it.throw(MyError('test error')))
+
+# When you call throw, the generator function may catch the injected exception with a standard try/except
+# compound statement that surrounds the last yield expression that wass executed.
