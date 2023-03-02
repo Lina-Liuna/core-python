1a19f2362770d697a6042b6b3d577b19d1264cda
Lina <liuna.lina@gmail.com>
2023-03-02 06:03:40 -0800

    generator-advanced-throw-method: inject try/except with yield

--- Effective_Python/ch4_item35_avoid_causing_state_transitions_in_generators_with_throw.py
+++ Effective_Python/ch4_item35_avoid_causing_state_transitions_in_generators_with_throw.py
@@ -17,13 +17,32 @@ def yield_iter():
     for i in range(1, 4):
         yield i
 
+
 def my_generator():
     yield from yield_iter()
+
 
 it = my_generator()
 print(next(it))
 print(next(it))
-print(it.throw(MyError('test error')))
+# print(it.throw(MyError('test error')))   # exception without try/except error
+
 
 # When you call throw, the generator function may catch the injected exception with a standard try/except
 # compound statement that surrounds the last yield expression that wass executed.
+def yield_iter_try():
+    for i in range(1, 4):
+        try:
+            yield i
+        except MyError:
+            print('Got MyError')
+
+
+def my_generator_try_except():
+    yield from yield_iter_try()
+
+
+it = my_generator_try_except()
+print(next(it))
+print(next(it))
+print(it.throw(MyError('test error')))
