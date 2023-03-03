f637915a6a2d377ab78bf598587d33a9edffdab4
Lina <liuna.lina@gmail.com>
2023-03-03 06:19:54 -0800

    itertool-module-recommened: important function list in itertools: 1. linking iterators together; 2. filtering items together; 3. producing combinations of item git push

---
+++ Effective_Python/ch4_item36_Consider_itertools_for_working_with_iterators_and_generators.py
@@ -0,0 +1,25 @@
+# The itertools built-in module contains a large number of functions that are useful for
+# organizing and itteracting with iterators
+
+import itertools
+
+# whenever you find yourself dealing with tricky iteration code, it's worthy looking at the itertools
+# The following sections describe the most important functions that you should know
+
+# 1. Linking iterators together
+    # 1. chain
+    # 2. repeat
+    # 3. cycle
+    # 4. tee
+    # 5. zip_longest
+# 2. Filtering items from iterator:
+    # 1. islice
+    # 2. takewhile
+    # 3. dropwhile
+    # 4. filterfalse
+# 3. Producing combinations of items from iterator:
+    # 1. accumulate
+    # 2. product
+    # 3. permutations
+    # 4. combinations
+    # 5. combinations_with_replacement
