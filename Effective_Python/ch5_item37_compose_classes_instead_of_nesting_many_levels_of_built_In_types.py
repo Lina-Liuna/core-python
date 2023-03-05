3363b7ade1cf59f5a4b50ccef307ae74ce24ebc8
Lina <liuna.lina@gmail.com>
2023-03-05 07:06:07 -0800

    object-oriented-program: inheritance/polymorphism/encapsulation/dynamic/ example_description

---
+++ Effective_Python/ch5_item37_compose_classes_instead_of_nesting_many_levels_of_built_In_types.py
@@ -0,0 +1,14 @@
+# object-oriented programming language: inheritance, polymorphism, and encapsulation.
+# python class: defining how classes interact through interfaces and hierarchies.
+
+# Dynamic:
+# how to maintaining dynamic iternal state?
+# pythons's dictionary type is wonderful for maintaining dynamic interal state over the lifetime of an object.
+
+# what is dynamic?
+# dynamic is a situation which you need to do bookkeeping for an unexpected set of identifiers.
+
+# Example:
+# Record the grades of a set of students whose names arent known in advance.
+# Can define a class to store the namex in a dictionary instead of using a predefined attribute for each student.
+
