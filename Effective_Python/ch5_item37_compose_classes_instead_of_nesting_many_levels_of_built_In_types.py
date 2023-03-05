b1e91fc2a01f5f4deaed645cdba94a0c08f20d8b
Lina <liuna.lina@gmail.com>
2023-03-05 07:17:22 -0800

    class-example: SimpleGradeBook to record the grades of a set of student

--- Effective_Python/ch5_item37_compose_classes_instead_of_nesting_many_levels_of_built_In_types.py
+++ Effective_Python/ch5_item37_compose_classes_instead_of_nesting_many_levels_of_built_In_types.py
@@ -12,3 +12,29 @@
 # Record the grades of a set of students whose names arent known in advance.
 # Can define a class to store the namex in a dictionary instead of using a predefined attribute for each student.
 
+class SimpleGradeBook:
+    def __init__(self):
+        self._grade = {}
+
+    def add_student(self, name):
+        self._grade[name] = []
+
+    def report_grade(self, name, score):
+        self._grade[name].append(score)
+
+    def averae_grade(self, name):
+        grades = self._grade[name]
+        return sum(grades) / len(grades)
+
+book = SimpleGradeBook()
+book.add_student('Lina Liu')
+book.report_grade('Lina Liu', 100)
+book.report_grade('Lina Liu', 99)
+book.report_grade('Lina Liu', 77)
+print(book.averae_grade('Lina Liu'))
+
+
+# problems:
+# dictionary is easy to use that there's a danger of overextending them to write brittle code.
+# if I want to extend the SimpleGradeBook class to keep a list of grades by subject, not just overall.
+# I can change the _grades dictionary to map student names to another dictionary
