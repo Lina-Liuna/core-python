156028927cda2f1db5d958e07762ed4ed48a6467
Lina <liuna.lina@gmail.com>
2023-03-06 06:29:21 -0800

    useful-example: Student Grade Book list by subject

--- Effective_Python/ch5_item37_compose_classes_instead_of_nesting_many_levels_of_built_In_types.py
+++ Effective_Python/ch5_item37_compose_classes_instead_of_nesting_many_levels_of_built_In_types.py
@@ -38,3 +38,32 @@ print(book.averae_grade('Lina Liu'))
 # dictionary is easy to use that there's a danger of overextending them to write brittle code.
 # if I want to extend the SimpleGradeBook class to keep a list of grades by subject, not just overall.
 # I can change the _grades dictionary to map student names to another dictionary
+import collections
+class BySubjectGradeBook:
+    def __init__(self):
+        self._grade = {}
+
+    def add_student(self, name):
+        self._grade[name] = collections.defaultdict(list)
+
+    def report_grade(self, name, subject, grade):
+        by_subject = self._grade[name]
+        grade_list = by_subject[subject]
+        grade_list.append(grade)
+
+    def average_grade(self, name):
+        by_subject = self._grade[name]
+        total, count = 0, 0
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+        return total / count
+
+book = BySubjectGradeBook()
+book.add_student('Lina Liu')
+book.report_grade('Lina Liu', 'math', 100)
+book.report_grade('Lina Liu', 'Reading', 90)
+book.report_grade('Lina Liu', 'oral english', 80)
+book.report_grade('Lina Liu', 'WorkOut', 99)
+print(book._grade)
+print(book.average_grade('Lina Liu'))
