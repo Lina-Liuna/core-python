c8faee8c9a090b0faaf3574537b1b66611e919af
Lina <liuna.lina@gmail.com>
2023-03-06 06:43:22 -0800

    useful-example: Student Grade Book list By subject and weighted scores

--- Effective_Python/ch5_item37_compose_classes_instead_of_nesting_many_levels_of_built_In_types.py
+++ Effective_Python/ch5_item37_compose_classes_instead_of_nesting_many_levels_of_built_In_types.py
@@ -65,5 +65,47 @@ book.report_grade('Lina Liu', 'math', 100)
 book.report_grade('Lina Liu', 'Reading', 90)
 book.report_grade('Lina Liu', 'oral english', 80)
 book.report_grade('Lina Liu', 'WorkOut', 99)
-print(book._grade)
+# print(book._grade)   # inner data with underscore, better not call it outside the class.
 print(book.average_grade('Lina Liu'))
+
+# Problem: I want to track the weight of each score toward the overall grade in the class.
+# add midterm and final exams and pop quizes.
+
+# Solution: change the innermost dictionary,
+# Instead of mapping subjects(its keys) to a list of grades(its values),
+# Use the tuple of (score, weight) in the values list:
+
+
+class WeightGradeBook:
+    def __init__(self):
+        self._grade = {}
+
+    def add_student(self, name):
+        self._grade[name] =collections.defaultdict(list)
+
+    def report_grade(self, name, subject, score, weight):
+        by_subject = self._grade[name]
+        grade_list = by_subject[subject]
+        grade_list.append((score, weight))
+
+    def average_grade(self, name):
+        by_subject = self._grade[name]
+
+        score_sum, score_count = 0, 0
+        for subject, scores, in by_subject.items():
+            subject_avg, total_weight = 0, 0
+            for score, weight in scores:
+                subject_avg += score * weight
+                total_weight += weight
+            score_sum += subject_avg / total_weight
+            score_count += 1
+
+        return score_sum / score_count
+
+book = WeightGradeBook()
+book.add_student('Lina')
+book.report_grade('Lina', 'Math', 90, 0.3)
+book.report_grade('Lina', 'Math', 98, 0.2)
+book.report_grade('Lina', 'WorkOut', 90, 0.2)
+book.report_grade('Lina', 'WorkOut', 99, 0.3)
+print(book.average_grade('Lina'))
