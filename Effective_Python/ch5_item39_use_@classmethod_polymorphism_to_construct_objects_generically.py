d8c15a539854d947fb33fe339bc6977fbd4567e5
Lina <liuna.lina@gmail.com>
2023-03-09 13:13:29 -0800

    use @classmethod polymorphism to construct objects generically

--- Effective_Python/ch5_item39_use_@classmethod_polymorphism_to_construct_objects_generically.py
+++ Effective_Python/ch5_item39_use_@classmethod_polymorphism_to_construct_objects_generically.py
@@ -99,8 +99,8 @@ def write_test_files(tmpdir):
 tmpdir = 'test_inputs'
 write_test_files(tmpdir)
 
-result = mapreduce(tmpdir)
-print(f'There are {result} lines')
+# result = mapreduce(tmpdir)
+# print(f'There are {result} lines')
 
 # Problem: mapreduce function is not generic at all.
 # If write another InputData or Worker subclass, I would also have to rewrite the genrate_inputs, create_workers,
@@ -116,9 +116,62 @@ print(f'There are {result} lines')
 # Solution: define class method polymorphism
 # class method polymorphism for whole classes instead of their constructed objects.
 
+# extend the InputData Class with a generic @classmethod that's responsible for creating new InputData instances
+# using a common interface:
 
 
+class GenericInputData:
+    def read(self):
+        raise NotImplementedError
+    @classmethod
+    def generate_inputs(cls, config):
+        raise NotImplementedError
 
+class PathInputData(GenericInputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
+
+    @classmethod
+    def generate_inputs(cls, config):
+        data_dir = config['data_dir']
+        for name in os.listdir(data_dir):
+            yield cls(os.path.join(data_dir, name))
+
+class GenericWorker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+    def map(self):
+        raise NotImplementedError
+    def reduce(self, other):
+        raise NotImplementedError
+    @classmethod
+    def create_workers(cls, input_class, config):
+        workers = []
+        for input_data in input_class.generate_inputs(config):
+            workers.append(cls(input_data))
+        return workers
+
+class LineCountWorker(GenericWorker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count('\n')
+
+    def reduce(self, other):
+        self.result += other.result
+
+def mapreduce(worker_class, input_class, config):
+    workers = worker_class.create_workers(input_class, config)
+    return execute(workers)
+
+config = {'data_dir': tmpdir}
+result = mapreduce(LineCountWorker, PathInputData, config)
+print(f'There are {result} lines')
 
 
 
