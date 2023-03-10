6c615672334bcd1ab5d2bfaf557873a86d957652
Lina <liuna.lina@gmail.com>
2023-03-10 07:37:57 -0800

    Problem-in-call__init__func-in-subclass: conflict may happen if the inheritance base class and the __init__ calls doesn't match the order

--- Effective_Python/ch5_item40_initialize_parent_classes_with_super.py
+++ Effective_Python/ch5_item40_initialize_parent_classes_with_super.py
@@ -12,3 +12,72 @@ class LinaLeftShoe:
 
 # THe above only works fine for basic class hirearchies but breaks in many cases.
 # if multiple inheritance calling the superclass __init__ method directly can lead to unpredictable behavior.
+
+
+# Problem: the __init__ call order isn't specified across all subclasses.
+# Define two parent classes that operate on the instance's value field.
+class BaseNumber:
+    def __init__(self, value):
+        self.value = value
+
+class TimesTwo:
+    def __init__(self):
+        self.value *= 2
+
+class PlusFive:
+    def __init__(self):
+        self.value += 5
+
+
+class CalcNumers(BaseNumber, TimesTwo, PlusFive):
+    def __init__(self, value):
+        BaseNumber.__init__(self, value)
+        TimesTwo.__init__(self)
+        PlusFive.__init__(self)
+
+
+CalcNum = CalcNumers(7)
+print('7 * 2 + 5 = ', CalcNum.value)
+
+# defines the same parent class but in a different order
+# PlusFive followed by TimesTwo
+# but the call inside __init__ in the same order as before
+# this class's behavior doesn't match the order of the parent classes in its definition.
+# the inheritance base classes and the __init__ calls is hard to spot.
+class CalcNumers_different_orders(BaseNumber, PlusFive, TimesTwo):
+    def __init__(self, value):
+        BaseNumber.__init__(self, value)
+        TimesTwo.__init__(self)
+        PlusFive.__init__(self)
+
+
+CalcNum = CalcNumers_different_orders(7)
+print('7 * 2 + 5 = ', CalcNum.value)
+
+# Diamond inheritance:
+# Diamond inheritance happens when a subclass inherits from two separate classes that have the same superclass
+# somewhere in the hierarchy.
+
+# Diamond inheritance causes the common superclass's __init__ method to run multiple times, causing unexpected behavior.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
