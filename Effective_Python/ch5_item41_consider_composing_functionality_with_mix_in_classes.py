48f2296b0a95d2064826d27e25e168d42dcc85b5
Lina <liuna.lina@gmail.com>
2023-03-10 19:47:04 -0800

    excellent-example: uses the mix-in to make a dictionary representation of a binary tree

---
+++ Effective_Python/ch5_item41_consider_composing_functionality_with_mix_in_classes.py
@@ -0,0 +1,78 @@
+# What if you want multiple inheritance, but want to avoid the potential headaches? writing a mix-in instead.
+
+# what is mix-in class?
+# A mix-in class is a class that defines only a small set of additional methods for its child classes to provide.
+# mix-in class don't define instance attributes and __init__ constructor.
+
+# python make mix-in to inspect the current state of any object, regardless of its type.
+
+# What does dynamic inspection mean?
+# Dynamic inspection means you can write generic functionlaity just once, in a mix-in, and it can then be applied
+# to many other classes.
+
+class ToDictMixin:
+    def to_dict(self):
+        return self._traverse_dict(self.__dict__)
+
+    def _traverse_dict(self, instance_dict):
+        output = {}
+        for key, value in instance_dict.items():
+            output[key] = self._traverse(key, value)
+        return output
+
+    def _traverse(self, key, value):
+        if isinstance(value, ToDictMixin):
+            return value.to_dict()
+        elif isinstance(value, dict):
+            return self._traverse_dict(value)
+        elif isinstance(value, list):
+            return [self._traverse(key, i) for i in value]
+        elif hasattr(value, '__dict__'):
+            return self._traverse_dict(value.__dict__)
+        else:
+            return value
+
+class BinaryTree(ToDictMixin):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+tree = BinaryTree(10,
+    left=BinaryTree(7, right=BinaryTree(9)),
+    right=BinaryTree(13, left=BinaryTree(11)))
+print(tree.to_dict())
+
+
+class ToDictMixin:
+    def to_dict(self):
+        return self._traverse_dict(self.__dict__)
+
+    def _traverse_dict(self, instance_dict):
+        output = {}
+        for key, value in instance_dict.items():
+            output[key] = self._traverse(key, value)
+        return output
+
+    def _traverse(self, key, value):
+        if isinstance(value, ToDictMixin):
+            return value.to_dict()
+        elif isinstance(value, dict):
+            return self._traverse_dict(value)
+        elif isinstance(value, list):
+            return [self._traverse(key, i) for i in value]
+        elif hasattr(value, '__dict__'):
+            return self._traverse_dict(value.__dict__)
+        else:
+            return value
+
+
+class BinaryTree(ToDictMixin):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+
