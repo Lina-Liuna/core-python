8ec017eb9254c39bff669d71e73f1004b1b9f8ba
Lina <liuna.lina@gmail.com>
2023-03-11 06:21:33 -0800

    advanced-mix-in-example: Jsonmixin: defines both instance methods and class method

--- Effective_Python/ch5_item41_consider_composing_functionality_with_mix_in_classes.py
+++ Effective_Python/ch5_item41_consider_composing_functionality_with_mix_in_classes.py
@@ -68,6 +68,68 @@ root.left.right = BinaryTreeWithParent(9, parent=root.left)
 print(root.to_dict())
 
 
+# Mix-ins can be composed together.
+# I want a mix-in that provides generic JSON serialization for any class.
+# I can do this by assuming that a class provides a to_dict method
+
+import json
+
+# JsonMixin class defines both instance methods and class method
+# Mix-ins let you add either kind of behavior to subclsses.
+# The only requirement of this example: providing a to_dict method and taking keyword arguments for the __init__ method
+# This Mix-in makes it simple to create hierarchies of utility classes that can be serialized to and from JSON
+# with little boilerplate.
+
+class JsonMixin:
+    @classmethod
+    def from_json(cls, data):
+        kwargs = json.loads(data)
+        return cls(**kwargs)
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+
+# I have a hierarchy of data classes reprenting parts of a datacenter topology.
+
+class DatacenterRack(ToDictMixin, JsonMixin):
+    def __init__(self, switch=None, machines=None):
+        self.switch = Switch(**switch)
+        self.machines = [
+            Machine(**kwargs) for kwargs in machines
+        ]
+
+
+class Switch(ToDictMixin, JsonMixin):
+    def __init__(self, ports=None, speed=None):
+        self.ports = ports
+        self.speed = speed
+
+
+class Machine(ToDictMixin, JsonMixin):
+    def __init__(self, cores=None, ram=None, disk=None):
+        self.cores = cores
+        self.ram = ram
+        self.disk = disk
+
+# Serializing these classes to and from JSON is simple.
+serialized = """{
+    "switch": {"ports": 5, "speed": 1e9},
+    "machines": [
+        {"cores": 8, "ram": 32e9, "disk": 5e12},
+        {"cores": 4, "ram": 16e9, "disk": 1e12},
+        {"cores": 2, "ram": 4e9, "disk": 500e9}
+] }"""
+
+deserialized = DatacenterRack.from_json(serialized)
+roundtrip = deserialized.to_json()
+
+json.loads(serialized)
+json.loads(roundtrip)
+
+assert json.loads(serialized) == json.loads(roundtrip)
+
+
 
 
 
