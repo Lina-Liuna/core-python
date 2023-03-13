4008ec142cb8636f6acb5dc046cd8787a93fb744
Lina <liuna.lina@gmail.com>
2023-03-13 06:03:06 -0700

    Hard-feature: how to make binary tree class act like a sequence type?

--- Effective_Python/ch5_item43_inherit_from_collections.abc_from_custom_container_types.py
+++ Effective_Python/ch5_item43_inherit_from_collections.abc_from_custom_container_types.py
@@ -21,3 +21,70 @@ print(workout.frequency())
 workout.pop()
 print(repr(workout))
 print(workout.frequency())
+
+
+# How to provide sequence sematics(like list or tuple) for a binary tree class
+
+
+# collections.abc module defines a set of abstract base classes that provide all of the typical method
+# for each container type.
+# when you subclass from those abstract base classes and forget to implement required methods,
+# the module tells you something is wrong.
+
+import collections.abc
+
+class BadType(collections.abc.Sequence):
+    pass
+
+# foo = BadType()  # TypeError: Can't instantiate abstract class BadType with abstract methods __getitem__, __len__
+
+# Example: # How to make binary tree class act like a sequence type?
+class BinaryNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class IndexableNode(BinaryNode):
+    def _traverse(self):
+        if self.left is not None:
+            yield  from self.left._traverse()
+        yield self
+        if self.right is not None:
+            yield from self.right._traverse()
+
+    def __getitem__(self, index):
+        for i, item in enumerate(self._traverse()):
+            if i == index:
+                return item.value
+        raise IndexError(f'Index {index} is out of range')
+
+
+class SequenceNode(IndexableNode):
+    def __len__(self):
+        max = 0
+        for count, _ in enumerate(self._traverse(), 1):
+            max = count
+            pass
+        return max
+
+
+
+class TreeNode(SequenceNode, collections.abc.Sequence):
+    pass
+
+tree = TreeNode(
+    10,
+    left=TreeNode(
+        5,
+        left=TreeNode(2),
+        right=TreeNode(
+            6,
+            right=TreeNode(7))),
+    right=TreeNode(
+        15,
+        left=TreeNode(11))
+)
+
+print('Index of 7 is', tree.index(7))
+print('count of 10 is', tree.count(10))
