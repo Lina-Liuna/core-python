a5b48f6d8d9be5ee630096f3089fc8bbfd0c093c
Lina <liuna.lina@gmail.com>
2023-03-13 07:04:59 -0700

    Advanced-class-feture: Example: @property and @XXX.setter to get and set attribute

--- Effective_Python/ch6_item44_use_plain_attributes_instead_of_setter_and_getter_method.py
+++ Effective_Python/ch6_item44_use_plain_attributes_instead_of_setter_and_getter_method.py
@@ -44,4 +44,42 @@ r2.voltage = 10
 print(f'After:  {r2.current:.2f} amps')
 
 
+# define a subclass that ensures all resistance values are above zeor ohms
+class BoundedResistance(Resistor):
+    def __init__(self, ohms):
+        super().__init__(ohms)
+
+    @property
+    def ohms(self):
+        return self._ohms
+
+    @ohms.setter
+    def ohms(self, ohms):
+        if ohms <= 0:
+            raise ValueError(f'ohms must be > 0; got {ohms}')
+        self._ohms = ohms
+
+r3 = BoundedResistance(1e3)
+r3.ohms = 0.01
+
+# use the @property to make attributes from parent classes immutable
+class FixedResistance(Resistor):
+    def __init__(self, ohms):
+        super().__init__(ohms)
+
+    @property
+    def ohms(self):
+        return self._ohms
+
+    @ohms.setter
+    def ohms(self, ohms):
+        if hasattr(self, '_ohms'):
+            raise AttributeError('Ohms is immutable')
+        self._ohms = ohms
+
+r4 = FixedResistance(1e3)
+# r4.ohms = 2e3   # AttributeError: Ohms is immutable
+
+# Never set attribute in get method.
+
 
