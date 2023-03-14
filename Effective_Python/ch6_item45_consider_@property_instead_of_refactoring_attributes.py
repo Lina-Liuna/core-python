6a8187674f294f8708e07e277ea0ae71ea360c09
Lina <liuna.lina@gmail.com>
2023-03-13 21:56:32 -0700

    advanced-@property-feature-example: leaky bucket quota leve on the fly.

---
+++ Effective_Python/ch6_item45_consider_@property_instead_of_refactoring_attributes.py
@@ -0,0 +1,71 @@
+# Advanced but common use of @property is: transitioning what was once a simple numerical attribute
+# into an on-the-fly calculation.
+
+# this is extremely helpful because it lets you migrate all existing usage of a class to have new behviors
+# without requiring any of the call sites to be rewritten.
+
+# @property provides an important stopgap for improving interfaces over time.
+
+# Example: Implement a leaky bucket quota.
+# the bucket class represents how much quota remains and the duration for which the quota will be available.
+import datetime
+
+class Bucket:
+    def __init__(self, period):
+        self.period_delta = datetime.timedelta(seconds=period)
+        self.reset_time = datetime.datetime.now()
+        self.max_quota = 0
+        self.quota_consumed = 0
+
+    def __repr__(self):
+        return(f'Bucket(max_quota={self.max_quota}'
+               f'quota_consumed={self.quota_consumed})')
+
+    # To match the previos interface of the original bucket class, I use @property method to compute the current
+    # level of quota on-the-fly using these new attributes
+    @property
+    def quota(self):
+        return self.max_quota - self.quota_consumed
+
+    @quota.setter
+    def quota(self, amount):
+        delta = self.max_quota - amount
+        if amount == 0:
+            # Quota being reset for a new period
+            self.quota_consumed = 0
+            self.max_quota = 0
+        elif delta < 0:
+            # Quota being filled for the new period
+            assert self.quota_consumed == 0
+            self.max_quota = amount
+        else:
+            # Quota being consumed during the period
+            assert self.max_quota >= self.quota_consumed
+            self.quota_consumed += delta
+
+
+def fill(bucket, amount):
+    now = datetime.datetime.now()
+    if (now - bucket.reset_time) > bucket.period_delta:
+        bucket.quota = 0
+        bucket.reset_time = now
+    bucket.quota += amount
+
+
+def deduct(bucket, amount):
+    now = datetime.datetime.now()
+    if (now - bucket.reset_time) > bucket.period_delta:
+        return False  # Bucket hasn't been filled this period
+    if bucket.quota - amount < 0:
+        return False  # Bucket was filled, but not enough
+    bucket.quota -= amount
+    return True  # Bucket had enough, quota consumed
+
+bucket = Bucket(60)
+print('Initial', bucket)
+fill(bucket, 100)
+print('Filled', bucket)
+if deduct(bucket, 99):
+    print('Had 99 quota')
+else:
+    print('Not enough for 99 quota')
