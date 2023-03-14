ab275021f4a70787712b222e73c7519ffcb4392e
Lina <liuna.lina@gmail.com>
2023-03-14 06:25:41 -0700

    @property-problem: example descriptor: write a class to validate student grade to check it's percentage

---
+++ Effective_Python/ch6_item46_use_descriptor_for_reuseable_@property_methods.py
@@ -0,0 +1,6 @@
+# What's the problem of @property?
+# @property methods it decorates can't be reused for multiple attributes of the same class.
+# @property methods it decorates can't be reused by unrelated classes.
+
+# Example:
+# I want a class to validate that the grade received by a student on a homework assignment is a percentage.
