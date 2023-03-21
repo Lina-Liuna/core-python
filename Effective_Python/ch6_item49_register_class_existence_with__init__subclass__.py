37e50704386c6ad3f01f8305f639d59e729a3705
Lina <liuna.lina@gmail.com>
2023-03-21 07:12:53 -0700

    super-useful-example: serialize/deserialize the JSON string and construct the Point2D object it represent

--- Effective_Python/ch6_item49_register_class_existence_with__init__subclass__.py
+++ Effective_Python/ch6_item49_register_class_existence_with__init__subclass__.py
@@ -28,3 +28,30 @@ class Point2D(Serializable):
 point = Point2D(5, 3)
 print('Object:    ', point)
 print('Serialized:', point.serialize())
+
+# Deserialize the JSON string and construct the Point2D object it represents.
+# define a class that can deserialize the data from its Serializable parent class:
+
+
+class Deserializable(Serializable):
+    @classmethod
+    def deserialize(cls, json_data):
+        params = json.loads(json_data)
+        return cls(*params['args'])
+
+class Point2D(Deserializable):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Point2D({self.x}, {self.y})'
+
+before = Point2D(5, 3)
+print('Before:    ', before)
+data = before.serialize()
+print('Serialized:', data)
+after = Point2D.deserialize(data)
+print('After:     ', after)
+
