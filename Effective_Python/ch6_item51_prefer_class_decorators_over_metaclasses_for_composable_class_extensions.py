fda148344dabe315926a1154dce1aca396212877
Lina <liuna.lina@gmail.com>
2023-03-25 08:55:00 -0700

    apply decorator to various special methods in the new dict subclass

--- Effective_Python/ch6_item51_prefer_class_decorators_over_metaclasses_for_composable_class_extensions.py
+++ Effective_Python/ch6_item51_prefer_class_decorators_over_metaclasses_for_composable_class_extensions.py
@@ -6,9 +6,8 @@ from functools import wraps
 
 
 def trace_func(func):
-    if hasattr(func, 'tracing'):
+    if hasattr(func, 'tracing'):  # Only decorate once
         return func
-
     @wraps(func)
     def wrapper(*args, **kwargs):
         result = None
@@ -20,9 +19,31 @@ def trace_func(func):
             raise
         finally:
             print(f'{func.__name__}({args!r}, {kwargs!r}) ->'
-                  f'{result!r}')
+                f'{result!r}')
 
-        wrapper.tracing = True
-        return wrapper
+    wrapper.tracing = True
+    return wrapper
+
+
+# apply this decorator to various special methods in the new dict subclass
+class TraceDict(dict):
+    @trace_func
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @trace_func
+    def __setitem__(self, *args, **kwargs):
+        return super().__setitem__(*args, **kwargs)
+
+    @trace_func
+    def __getitem__(self, *args, **kwargs):
+        return super().__getitem__(*args, **kwargs)
+
+
+# trace_dict = TraceDict({'hi', 1})  # Error
+trace_dict = TraceDict([('LinaMe', 2), ('Sat', 8)])
+trace_dict['weather'] = 'good'
+trace_dict['LinaMe'] = 'hi'
+
 
 
