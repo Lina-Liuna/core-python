624ba632f18497b904db812932ca7a62da047ae6
Lina <liuna.lina@gmail.com>
2023-03-28 05:58:49 -0700

    basic-concept: GIL(global interpreter lock): prevent interrruption and ensure every bytecode instruction works correctly

--- Effective_Python/ch7_item53_use_threads_for_blocking_io_avoid_for_parallelism.py
+++ Effective_Python/ch7_item53_use_threads_for_blocking_io_avoid_for_parallelism.py
@@ -8,4 +8,18 @@
 
 # CPython enforces coherence with a mechanism called the global interpreter lock(GIL)
 
+# GIL:
+# Global interpreter lock
+# GIL is a mutual-exculsion lock(mutex) that prevents CPython from being affected
+# by preemptive multithreading.
+
+# GIL prevent one thread may interrupting another thread interpreter state at an
+# unexpected time.
+
+# GIL prevent the interruptions and ensures that every bytecode instruction works correctly
+# with CPython implementation and its C-extension modules.
+
+# GIL negative side effect:
+# GIL reach for threads to do parallel computation and this may not speed up your python programs.
+
 
