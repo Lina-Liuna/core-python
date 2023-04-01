698dc12b8766cd803b180eebcb98180a63ba8ada
Lina <liuna.lina@gmail.com>
2023-04-01 06:24:14 -0700

    3-phase-pipeline-consumer-4-queue-producer-1000-works: final-solution

--- Effective_Python/ch7_item55_use_queue_to_coordinate_work_between_threads.py
+++ Effective_Python/ch7_item55_use_queue_to_coordinate_work_between_threads.py
@@ -240,7 +240,67 @@ in_queue.join()
 print("Producer done")
 thread.join()
 
+# define a Queue subclass to put all behavior together to
+# tell the worker thread when it should stop processing
+
+class ClosableQueue(Queue):
+    SENTINEL = object()
+
+    # define a close method that adds a special sentinel item to the queue
+    # the sentinel item indicates there will be no more input items after it.
+    def close(self):
+        self.put(self.SENTINEL)
 
 
+    # define an iterator for the queue thtat looks for this special object
+    # and stop iteration when its found.
+    def __iter__(self):
+        while True:
+            item = self.get()
+            try:
+                if item is self.SENTINEL:
+                    return
+                yield item
+            finally:
+                self.task_done()
 
 
+class StoppableWorker(Thread):
+    def __init__(self, func, in_queue, out_queue):
+        super().__init__()
+        self.func = func
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+
+    def run(self):
+        for item in self.in_queue:
+            result = self.func(item)
+            self.out_queue.put(result)
+
+
+download_queue = ClosableQueue()
+resize_queue = ClosableQueue()
+upload_queue = ClosableQueue()
+done_queue = ClosableQueue()
+threads = [
+    StoppableWorker(download, download_queue, resize_queue),
+    StoppableWorker(resize, resize_queue, upload_queue),
+    StoppableWorker(upload, upload_queue, done_queue),
+]
+
+
+for thread in threads:
+    thread.start()
+
+for _ in range(1000):
+    download_queue.put(object())
+download_queue.close()
+
+download_queue.join()
+resize_queue.close()
+resize_queue.join()
+upload_queue.close()
+upload_queue.join()
+print(done_queue.qsize(), 'items finished')
+for thread in threads:
+    thread.join()
