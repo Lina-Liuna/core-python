7a8a9f17a9154a93f5ca6f99700c14102038f908
Lina <liuna.lina@gmail.com>
2023-07-16 12:04:11 -0700

    change the file name to news title name

--- tiny-tools/get_all_video_cc_on_youtubewebpage.py
+++ tiny-tools/get_all_video_cc_on_youtubewebpage.py
@@ -6,18 +6,9 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 
 
-ydl_opts = {
-    'nocheckcertificate': True,
-    'writesubtitles': True,
-    'allsubtitles:': True,
-    'writeautomaticsub': True,
-    'skip_download': True,
-    'listsubtitles': True,
-    'subtitleslangs': ['en'],
 
-}
 # Make an HTTP request to the website
-youtubesite = "https://www.youtube.com/@abc7news/videos?view=0&sort=dd&shelf_id=0"  # Replace with the URL of the website you want to parse
+youtubesite = "https://www.youtube.com/@abc7news/videos"  # Replace with the URL of the website you want to parse
 
 
 def get_all_matches(url):
@@ -25,29 +16,31 @@ def get_all_matches(url):
     content = response.content.decode("utf-8")  # Replace "utf-8" with the correct encoding if needed
     # Find the string starting with "/watch?v=" and ending with """
     matches = re.findall(r'"/watch\?v=([^"]+)"', content)
-
     # Print the matches
     for match in matches:
         print('https://www.youtube.com/watch?v=' + match)
-    return matches
+    titles = re.findall(r'"title":{"runs":\[\{"text":"(.*?)"\}\],', content)
 
-youtube_links = get_all_matches(youtubesite)
+    return (titles, matches)
+
+(youtube_titles, youtube_links) = get_all_matches(youtubesite)
 
 
 # the parameter is in url string after character v=
 def get_cc(filename, urllink):
     srt = YouTubeTranscriptApi.get_transcript(urllink)
+
+
     with open(filename, 'w') as f:
         for item in srt:
             f.write(list(item.values())[0], )
             f.write('\n')
 
-    print(list(srt))
+def write_all_cc_to_file(titles, urlinks):
+    for title, linkstr in zip(titles, urlinks):
+        print(title)
+        get_cc('/Users/linaliu/code/Booklist/gallery/youtube/newstoday/'+ title +'.txt', linkstr)
 
-def write_all_cc_to_file(urlinks):
-    for linkstr in urlinks:
-        get_cc('/Users/linaliu/code/core-python/tiny-tools/youtube_cc/ABC7_News_Bay_Area_today_cc/'+linkstr+'.txt', linkstr)
 
-
-write_all_cc_to_file(youtube_links)
+write_all_cc_to_file(youtube_titles,youtube_links)
 
